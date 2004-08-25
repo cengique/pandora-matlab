11fa5af14911d887661074b7115d3d0d5d155065
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2004-08-25 16:37:08 +0000

    Initial revision
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@13 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @period/subsref.m
@@ -0,0 +1,16 @@
+function b = subsref(a,index)
+% subsref - Defines generic indexing for objects.
+if size(index, 2) > 1
+  first = subsref(a, index(1));
+  %# recursive
+  b = subsref(first, index(2:end));
+else
+  switch index.type
+    case '()'
+      b = a(index.subs{:});
+    case '.'
+      b = eval(['a.' index.subs]);
+    case '{}'
+      b = a{index.subs{:}};
+  end
+end
