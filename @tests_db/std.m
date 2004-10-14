666ff989d5a9ca488651468dda5de59f036fab27
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2004-10-14 21:16:24 +0000

    Made it work on N-D arrays by uding recursion. It's slow.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@58 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/std.m
+++ @tests_db/std.m
@@ -1,11 +1,14 @@
 function s = std(db, flag, dim)
 
-% std - Returns the std of the data matrix of db.
+% std - Returns the std of the data matrix of db. Ignores NaN values.
 %
 % Usage:
 % s = std(db, flag, dim)
 %
 % Description:
+%   Does a recursive operation over dimensions in order to remove NaN values.
+% This takes considerable amount of time compared with a straightforward std
+% operation. 
 %
 %   Parameters:
 %	db: A tests_db object.
@@ -28,20 +31,40 @@ if ~ exist('flag')
 end
 
 %# Always do row stds
-if dim == 2
-  data = db.data';
+order = 1:length(size(db.data));
+if dim ~= 1
+  order(dim) = 1;
+  order(1) = dim;
+  data = permute(db.data, order);
 else
   data = db.data;
 end
 
 %# Allocate results array
-s = repmat(NaN, [1 size(data, 2)]);
+db_size = size(data);
+s = repmat(NaN, [1 db_size(2:end)]);
+
+%# Do a loop over EACH other dimension (!)
+s = recstd(data, length(db_size));
 
 %# Do a loop over the other dimension
-for num=1:size(data, 2)
-  s(num) = std(data(~isnan(data(:,num)), num), flag, 1);
+%#for num=1:size(data, 2)
+%#  s(num) = std(data(~isnan(data(:,num)), num), flag, 1);
+%#end
+
+if dim ~= 1
+  s = ipermute(s, order);
 end
 
-if dim == 2
-  s = s';
-end
+%# Recursive std needed for stripping NaNs in each dimension
+function s = recstd(data, dim)
+  if dim == 1
+    s = std(data(~isnan(data(:))), flag, 1);
+  else
+    for num=1:size(data, dim)
+      %# Otherwise recurse
+      [dims{1:(dim-1)}] = deal(':');
+      dims{dim} = num;
+      s(dims{:}) = recstd(data(dims{:}), dim - 1);
+    end
+  end
