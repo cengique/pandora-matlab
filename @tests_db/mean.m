44664e58ee325e7be0920fb629a3f41a3dab0275
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2004-10-14 21:16:57 +0000

    Made it work for N-D arrays.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@59 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/mean.m
+++ @tests_db/mean.m
@@ -1,11 +1,14 @@
 function s = mean(db, dim)
 
-% mean - Returns the mean of the data matrix of db.
+% mean - Returns the mean of the data matrix of db. Ignores NaN values.
 %
 % Usage:
 % s = mean(db, dim)
 %
 % Description:
+%   Does a recursive operation over dimensions in order to remove NaN values.
+% This takes considerable amount of time compared with a straightforward mean 
+% operation. 
 %
 %   Parameters:
 %	db: A tests_db object.
@@ -23,21 +26,37 @@ if ~ exist('dim')
   dim = 1; %# Go down rows by default
 end
 
-%# Always do row means
-if dim == 2
-  data = db.data';
+%# Always do row-wise
+order = 1:length(size(db.data));
+if dim ~= 1
+  order(dim) = 1;
+  order(1) = dim;
+  data = permute(db.data, order);
 else
   data = db.data;
 end
 
 %# Allocate results array
-s = repmat(NaN, [1 size(data, 2)]);
+db_size = size(data);
+s = repmat(NaN, [1 db_size(2:end)]);
 
-%# Do a loop over the other dimension
-for num=1:size(data, 2)
-  s(num) = mean(data(~isnan(data(:,num)), num), 1);
+%# Do a loop over EACH other dimension (!)
+s = recmean(data, length(db_size));
+
+if dim ~= 1
+  s = ipermute(s, order);
 end
 
-if dim == 2
-  s = s';
-end
+%# Recursive std needed for stripping NaNs in each dimension
+function s = recmean(data, dim)
+  if dim == 1
+    s = mean(data(~isnan(data(:))), 1);
+  else
+    for num=1:size(data, dim)
+      %# Otherwise recurse
+      [dims{1:(dim-1)}] = deal(':');
+      dims{dim} = num;
+      s(dims{:}) = recmean(data(dims{:}), dim - 1);
+    end
+  end
+
