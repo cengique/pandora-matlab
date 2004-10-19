89420ac0be1ca9b2688c5706ecdb3fc9b550ffa9
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2004-10-19 21:03:31 +0000

    Finally got the layout to work correctly. Found a bunch of bugs that impeded the results.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@62 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @plot_stack/plot.m
+++ @plot_stack/plot.m
@@ -29,31 +29,120 @@ end
 
 axes('position', layout_axis);
 
+left_side = layout_axis(1);
+bottom_side = layout_axis(2);
+width = layout_axis(3);
+height = layout_axis(4);
+
 %# Divide the layout area according to number of plots contained
 num_plots = length(a_plot.plots);
 
 scale_down = 0.95;
 border = 1;
 
-%# Find the width of a regular y-axis label
-ticklabel = text(0, 0, 'xxx', 'Visible', 'off');
-tickextent = get(ticklabel, 'Extent');
-tickwidth = 0.03; %#tickextent(3)
-tickheight = 0.03; %#tickextent(4)
-labelwidth = 0.03;
-labelheight = 0.03;
+%# Find the width of a regular y-axis label (DOESN'T WORK!)
+%#ticklabel = text(0, 0, 'xxx', 'Visible', 'off');
+%#tickextent = get(ticklabel, 'Extent');
+
+%# Fixed size for ticks and labels
+decosize = 0.03;
+
+a_plot.props
+
+%# Handle special label and tick placements to create maximal plotting area
+if isfield(a_plot.props, 'xLabelsPos') 
+  switch a_plot.props.xLabelsPos
+      case 'bottom'
+	bottom_side = bottom_side + decosize / 2;
+	height = height - decosize / 2;
+	labelheight = 0;
+      case 'none'
+	labelheight = 0;
+      otherwise
+	error(['xLabelsPos=' a_plot.props.xLabelsPos ' not recognized.' ]);
+    end
+else
+  %# Check if a parent stacker already separated space for the decorations
+  if isfield(a_plot.props, 'noXLabel') && a_plot.props.noXLabel == 0
+    labelheight = 0;
+  else
+    labelheight = decosize / 2;
+  end
+end
+
+if isfield(a_plot.props, 'xTicksPos') 
+  switch a_plot.props.xTicksPos
+      case 'bottom'
+	bottom_side = bottom_side + decosize / 2;
+	height = height - decosize / 2;
+	tickheight = 0;
+      case 'none'
+	tickheight = 0;
+      otherwise
+	error(['xTicksPos=' a_plot.props.xTicksPos ' not recognized.' ]);
+    end
+else
+  if isfield(a_plot.props, 'noXTickLabels') && a_plot.props.noXTickLabels == 0
+    tickheight = 0;
+  else
+    tickheight = decosize / 2;
+  end
+end
+
+if isfield(a_plot.props, 'yLabelsPos') 
+  switch a_plot.props.yLabelsPos
+      case 'left'
+	left_side = left_side + decosize;
+	width = width - decosize;
+	labelwidth = 0;
+      case 'none'
+	labelwidth = 0;
+      otherwise
+	error(['yLabelsPos=' a_plot.props.yLabelsPos ' not recognized.' ]);
+    end
+else
+  if isfield(a_plot.props, 'noYLabel') && a_plot.props.noYLabel == 0
+    labelwidth = 0;
+  else
+    labelwidth = decosize;
+  end
+end
+
+if isfield(a_plot.props, 'yTicksPos') 
+  switch a_plot.props.yTicksPos
+      case 'left'
+	left_side = left_side + decosize;
+	width = width - decosize;
+	tickwidth = 0;
+      case 'none'
+	tickwidth = 0;
+      otherwise
+	error(['yTicksPos=' a_plot.props.yTicksPos ' not recognized.' ]);
+    end
+else
+  if isfield(a_plot.props, 'noYTickLabels') && a_plot.props.noYTickLabels == 0
+    tickwidth = 0;
+  else
+    tickwidth = decosize;
+  end
+end
+
+labelwidth
+labelheight
+tickwidth
+tickheight
 
 if strcmp(a_plot.orient, 'x')
-  width = border * layout_axis(3) / num_plots;
-  height = border * layout_axis(4);
+  tilewidth = border * width / num_plots;
+  tileheight = border * height;
 elseif strcmp(a_plot.orient, 'y')
-  width = border * layout_axis(3);
-  height = border * layout_axis(4) / num_plots;
+  tilewidth = border * width;
+  tileheight = border * height / num_plots;
 end
 
 %# Put a title first
-text(layout_axis(1) + layout_axis(3) / 2, ...
-     layout_axis(2) + layout_axis(4) + 0.1, ...
+text(left_side + width / 2, ...
+     bottom_side + height + 0.1, ...
      get(a_plot, 'title'), ...
      'Units', 'Normalized', ...
      'HorizontalAlignment', 'center', 'VerticalAlignment', 'top' );
@@ -73,46 +162,48 @@ for plot_num=1:num_plots
 	((plot_num > 1 && strcmp(a_plot.props.yTicksPos, 'left')) || ...
 	 strcmp(a_plot.props.yTicksPos, 'none'))
     its_props(1).noYTickLabels = 1;
-    tickwidth = 0; %# no more space is needed for y-tick labels
+    %#tickwidth = 0; %# no more space is needed for y-tick labels
   end
   if isfield(a_plot.props, 'yLabelsPos') && ...
 	((plot_num > 1 && strcmp(a_plot.props.yLabelsPos, 'left')) || ...
 	 strcmp(a_plot.props.yLabelsPos, 'none'))
     its_props(1).noYLabel = 1;
     %#tickwidth = 0; %# no more space is needed for y-tick labels
-    labelwidth = 0;
+    %#labelwidth = 0;
   end
   if isfield(a_plot.props, 'xTicksPos') && ...
 	((plot_num > 1 && strcmp(a_plot.props.xTicksPos, 'bottom')) || ...
 	 strcmp(a_plot.props.xTicksPos, 'none'))
     its_props(1).noXTickLabels = 1;
-    tickheight = 0; %# no more space is needed for x-tick labels
+  else
+    its_props(1).noXTickLabels = 0
   end
   if isfield(a_plot.props, 'xLabelsPos') && ...
-	((plot_num > 1 && strcmp(a_plot.props.xLabelsPos, 'left')) || ...
+	((plot_num > 1 && strcmp(a_plot.props.xLabelsPos, 'bottom')) || ...
 	 strcmp(a_plot.props.xLabelsPos, 'none'))
     its_props(1).noXLabel = 1;
-    %#tickheight = 0; %# no more space is needed for x-tick labels
-    labelheight = 0;
+  else
+    %# Signal to plot that it has space to put its labels
+    its_props(1).noXLabel = 0
   end
   %# Check if title only for the topmost plot
   if isfield(a_plot.props, 'titlesPos') 
     if 	(plot_num < num_plots && strcmp(a_plot.props.titlesPos, 'top')) || ...
 	  strcmp(a_plot.props.titlesPos, 'none')
-      one_plot = set(one_plot, 'title', '');
+      its_props(1).noTitle = 1;
     end
   end
   %# Set the modified properties back to the plot
   one_plot = set(one_plot, 'props', its_props);
   %# Calculate subplot positioning
-  x_offset = layout_axis(1) + tickwidth + (1 - scale_down) * width / 2 + ...
-      strcmp(a_plot.orient, 'x') * (plot_num - 1) * width;
-  y_offset = layout_axis(2) + tickheight + (1 - scale_down) * height / 2 + ...
-      strcmp(a_plot.orient, 'y') * (plot_num - 1) * height;
+  x_offset = left_side + (1 - scale_down) * tilewidth / 2 + ...
+      strcmp(a_plot.orient, 'x') * (plot_num - 1) * tilewidth;
+  y_offset = bottom_side + (1 - scale_down) * tileheight / 2 + ...
+      strcmp(a_plot.orient, 'y') * (plot_num - 1) * tileheight;
   position = [x_offset, y_offset, ...
-	      max(scale_down * width - tickwidth - labelwidth, minwidth), ...
-	      max(scale_down * height - tickheight - labelheight, minheight) ];
-  plot(a_plot.plots{plot_num}, position);
+	      max(scale_down * tilewidth - tickwidth - labelwidth, minwidth), ...
+	      max(scale_down * tileheight - tickheight - labelheight, minheight) ]
+  plot(one_plot, position);
   %# Set its axis limits if requested
   if ~ isempty(a_plot.axis_limits)
     axis(a_plot.axis_limits);
