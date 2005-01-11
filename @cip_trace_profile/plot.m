3e8f462a7afce938ecb706f2b5f4da0e1ebd9a01
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-01-11 20:56:00 +0000

    Now displays pulse spike shape, too.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@94 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @cip_trace_profile/plot.m
+++ @cip_trace_profile/plot.m
@@ -26,13 +26,22 @@ else
   ht = plot(t.trace);
   hsp = plot(t.spikes);
   %# TODO: Hack, fix it
-  s = t.spont_spike_shape
-  if ~ isempty(s.trace.data) 
-    hss1 = plotFigure(plotResults(t.spont_spike_shape));
-    hss2 = plotFigure(plotTPP(t.spont_spike_shape));
+  [hss1 hss2] = plotSpikeShape(t.spont_spike_shape, 'Spont');
+  [hss1 hss2] = plotSpikeShape(t.pulse_spike_shape, 'Pulse');
+
+  super_plot = set(superposePlots([plotResults(t.spont_spike_shape) ...
+				   plotResults(t.pulse_spike_shape)]), 'legend', {});
+  plotFigure(super_plot);
+  h = [ht, hsp, hss1, hss2];
+end
+
+function [hss1, hss2] = plotSpikeShape(spsh, name)
+  if ~ isempty(spsh.trace.data) 
+    pr = plotResults(spsh);
+    pr = set(pr, 'title', [ name ' ' get(pr, 'title')] )
+    hss1 = plotFigure(pr);
+    hss2 = plotFigure(plotTPP(spsh));
   else
     hss1 = NaN;
     hss2 = NaN;
   end
-  h = [ht, hsp, hss1, hss2];
-end
