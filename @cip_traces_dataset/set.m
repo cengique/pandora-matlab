baabec24ca80c2bd584771795d2bf0fdc20cc788
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-01-14 18:17:05 +0000

    Minor improvement.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@111 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @cip_traces_dataset/set.m
+++ @cip_traces_dataset/set.m
@@ -2,7 +2,7 @@ function a = set(a, attr, val)
 % set - Generic method for setting object attributes.
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
 try
-  eval(['a.' attr ' = val;']);
+  a.(attr) = val;
 catch
   a.params_tests_dataset = set(a.params_tests_dataset, attr, val);
 end
--- @params_tests_dataset/set.m
+++ @params_tests_dataset/set.m
@@ -1,8 +1,8 @@
 function a = set(a, attr, val)
 % set - Generic method for setting object attributes.
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
-try
-  eval(['a.' attr ' = val;']);
-catch
-  rethrow(lasterror);
-end
+%#try
+  a.(attr) = val;
+%#catch
+  %#rethrow(lasterror);
+%#end
