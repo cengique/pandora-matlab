7b71f9aa3f116b0463368766a78de72a9cc291e7
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-01-14 18:17:32 +0000

    Added support to display pulse spike shapes.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@112 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_tests_db/displayRankingsTeX.m
+++ @params_tests_db/displayRankingsTeX.m
@@ -51,7 +51,6 @@ if ranked_num_rows > 0
   num_best = 50;
   top_ranks = onlyRowsTests(joined_db, 1:min(num_best, ranked_num_rows), ':', ':');
   p_hists = paramsHists(top_ranks);
-  p_hists(1)
   plotFigure(plot_stack(num2cell(plotEqSpaced(p_hists)), [], 'x', ...
 			['Parameter distribution histograms of ' num2str(num_best) ...
 			 ' best matches' ], ...
@@ -81,33 +80,52 @@ if ranked_num_rows > 0
     %# prepare a landscape figure with two rows
     %# one row for original and 5 matching data traces
     %# second row for original and 5 matching spike shapes
-    %# TODO: get to ItemIndex somehow
+    %# TODO: This should be in a *_profile class
     orig_prof = loadItemProfile(props.crit_dataset, get(onlyRowsTests(crit_db, 1, 'ItemIndex'), 'data'));
     trace_plots = cell(1, 6);
-    shape_plots = cell(1, 6);
+    spont_shape_plots = cell(1, 6);
+    pulse_shape_plots = cell(1, 6);
     trace_plots{1} = plotData(orig_prof.trace);
 
-    results  = getResults(orig_prof.spont_spike_shape);
-    shape_plots{1} = setProp(plotResults(orig_prof.spont_spike_shape), 'axisLimits', ...
-			     [0, results.MinTime * 1.1, NaN, NaN]);
+    spont_results  = getResults(orig_prof.spont_spike_shape);
+    spont_xmax = spont_results.MinTime * 1.1;
+    spont_shape_plots{1} = set(plotResults(orig_prof.spont_spike_shape), ...
+			       'title', 'Criterion');
+
+    pulse_results  = getResults(orig_prof.pulse_spike_shape);
+    pulse_xmax = pulse_results.MinTime * 1.1;
+    pulse_shape_plots{1} = plotResults(orig_prof.pulse_spike_shape);
+    %# = setProp(, 'title', 'Criterion');
+
     for plot_num=1:5
+      rank_num = (plot_num - 1) * 10 + 1;
       prof = loadItemProfile(props.a_dataset, ...
-			     get(onlyRowsTests(joined_db, (plot_num - 1) * 10 + 1 , ...
+			     get(onlyRowsTests(joined_db, rank_num , ...
 					       'ItemIndex'), 'data'));
       trace_plots{1 + plot_num} = plotData(prof.trace);
-      results  = getResults(prof.spont_spike_shape);
-      shape_plots{1 + plot_num} = ...
-	  setProp(plotResults(prof.spont_spike_shape), ...
-		  'axisLimits', [0, results.MinTime * 1.1, NaN, NaN]);
-    end
-    horiz_props = struct('titlesPos', 'none', 'yLabelsPos', 'left');
-    top_row_plot = plot_stack(trace_plots, [], 'x', ...
-			      'Criterion and 5 matching traces', horiz_props);
-    bottom_row_plot = plot_stack(shape_plots, [], 'x', ...
-				 'Criterion and 5 matching spike shapes', horiz_props);
+      spont_results  = getResults(prof.spont_spike_shape);
+      spont_xmax = max(spont_xmax, spont_results.MinTime * 1.1);
+      spont_shape_plots{1 + plot_num} = ...
+	  set(plotResults(prof.spont_spike_shape), ...
+	      'title', ['Rank ' num2str(rank_num) ]);
 
-    plotFigure(plot_stack({top_row_plot, bottom_row_plot}, [], 'y', '', ...
-			  struct('titlesPos', 'none')));
+      pulse_results  = getResults(prof.pulse_spike_shape);
+      pulse_xmax = max(pulse_xmax, pulse_results.MinTime * 1.1);
+      pulse_shape_plots{1 + plot_num} = plotResults(prof.pulse_spike_shape);
+    end
+    horiz_props = struct('titlesPos', 'none', 'yLabelsPos', 'left', 'xLabelsPos', 'none');
+    top_row_plot = plot_stack(trace_plots, [], 'x', ...
+			      'Raw traces', ...
+			      mergeStructs(struct('xLabelsPos', 'bottom'), horiz_props));
+    bottom_row_plot2 = plot_stack(pulse_shape_plots, [0, pulse_xmax, NaN, NaN], 'x', ...
+				 'Pulse spike shapes', ...
+				 horiz_props);
+    bottom_row_plot = plot_stack(spont_shape_plots, [0, spont_xmax, NaN, NaN], 'x', ...
+				 'Spontaneous spike shapes', ...
+				 mergeStructs(struct('titlesPos', 'all'), horiz_props));
+
+    plotFigure(plot_stack({top_row_plot, bottom_row_plot2, bottom_row_plot}, ...
+			  [], 'y', '', struct('titlesPos', 'none')));
 
     filename = [ get(crit_db, 'id') ' - some matching raw traces' ];
     %# Replace all white space with underscores
