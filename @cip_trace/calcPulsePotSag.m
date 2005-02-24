f384607d9e5e20aa90f53a89048c103744cc28ca
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-02-24 21:17:04 +0000

    Changed from error to warning.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@140 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @cip_trace/calcPulsePotSag.m
+++ @cip_trace/calcPulsePotSag.m
@@ -30,5 +30,6 @@ steady_val = calcAvg(t.trace, periodPulseIni50msRest2(t));
 sag_val = steady_val - min_val;
 
 if (sag_val < 0)
-  error('Negative sag! Make sure there''re no spikes in CIP period.')
+  warning('Negative sag! Make sure there''re no spikes in CIP period.');
+  sag_val = NaN;
 end
