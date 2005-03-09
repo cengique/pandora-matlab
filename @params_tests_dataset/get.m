65b27c7058fa7a1c37d4e162a937f843bcba3a3c
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-03-09 19:46:38 +0000

    Improved.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@148 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_tests_dataset/get.m
+++ @params_tests_dataset/get.m
@@ -1,8 +1,4 @@
 function b = get(a, attr)
 % get - Defines generic attribute retrieval for objects.
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/14
-try
-  b = getfield(struct(a), attr);
-catch
-  rethrow(lasterror);
-end
+b = getfield(struct(a), attr);
