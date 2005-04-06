bd705667e4c38dc1abc689e9e4ee84eb6d6ffb87
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-04-06 20:45:37 +0000

    Added tolerateNaNs prop.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@181 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/rankMatching.m
+++ @tests_db/rankMatching.m
@@ -1,4 +1,4 @@
-function a_ranked_db = rankMatching(db, crit_db)
+function a_ranked_db = rankMatching(db, crit_db, props)
 
 % rankMatching - Create a ranking db of row distances of db to given criterion db.
 %
@@ -11,6 +11,8 @@ function a_ranked_db = rankMatching(db, crit_db)
 %   Parameters:
 %	db: A tests_db to rank.
 %	crit_db: A tests_db object holding the match criterion tests and stds.
+%	props: A structure with any optional properties.
+%	  tolerateNaNs: If 0, rows with any NaN values are skipped (default=1).
 %		
 %   Returns:
 %	a_ranked_db: A ranked_db object.
@@ -19,6 +21,10 @@ function a_ranked_db = rankMatching(db, crit_db)
 %
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/12/08
+
+if ~ exist('props')
+  props = struct([]);
+end
 
 %# If not exists, add RowIndex column
 if isfield(db.col_idx, 'RowIndex')
@@ -33,6 +39,11 @@ itemIndices = strmatch('ItemIndex', crit_tests);
 
 %# Strip out the NeuronId, RowIndex, and ItemIndex columns from criterion db
 crit_tests = setdiff(crit_tests, {'NeuronId', 'RowIndex', crit_tests{itemIndices}});
+
+%# Use only non-NaN values available in the criterion
+crit_row_db = onlyRowsTests(crit_db, 1, crit_tests);
+nonnans = ~ isnan(crit_row_db.data);
+crit_tests = crit_tests(nonnans);
 
 %# Filter relevant columns
 reduced_db = db(':', crit_tests);
@@ -55,9 +66,13 @@ wghd_data = diff_data ./ rep_weight;
 %# Sum of squares: distance measure
 %# Look for NaN values, skip them and count the non-NaN values to normalize the SS
 ss_data = wghd_data .* wghd_data;
-nans = isnan(ss_data);
-ss_data(nans) = 0; %# Replace NaNs with 0s
-summed_data = sum(ss_data, 2) ./ sum(~nans, 2); %# Sum distances and normalize by non-NaNs
+if ~ isfield(props, 'tolerateNaNs') || props.tolerateNaNs == 1
+  nans = isnan(ss_data);
+  ss_data(nans) = 9; %# Replace NaNs with 0s, 3 for 3 STDs different
+  summed_data = sum(ss_data, 2) ./ sum(~nans, 2); %# Sum distances and take average of non-NaNs
+else
+  summed_data = sum(ss_data, 2) ./ size(ss_data, 2);
+end
 
 %# Ignore NaN rows (there will be non NaN rows after the above)
 nans = isnan(summed_data);
@@ -68,4 +83,5 @@ wghd_data = wghd_data(~nans, :);
 %# Create a ranked_db with distances and row indices, sorted with distances
 a_ranked_db = ranked_db([wghd_data, summed_data, row_index], ...
 			{crit_tests{:}, 'Distance', 'RowIndex'}, db, crit_db, ...
-			[ lower(get(db, 'id')) ' ranked to ' lower(get(crit_db, 'id')) ]);
+			[ lower(get(db, 'id')) ' ranked to ' ...
+			 lower(get(crit_db, 'id')) ], props);
