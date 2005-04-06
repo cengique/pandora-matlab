b78ec7a618e2199ee473dc40c0e8370114bcf759
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-04-06 20:34:41 +0000

    Additional decorations and some controls.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@169 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @ranked_db/blockedDistances.m
+++ @ranked_db/blockedDistances.m
@@ -1,11 +1,13 @@
-function a_db = blockedDistances(a_ranked_db, rows, blocked_db, blocked_param_indices, ...
-				 block_levels, crit_db)
+function [a_db, ranked_dbs] = ...
+      blockedDistances(a_ranked_db, rows, blocked_db, blocked_param_indices, ...
+		       block_levels, crit_db)
 
 % blockedDistances - Creates a db of distances to blocked versions of top ranks.
 %
 % Usage:
-% a_db = blockedDistances(a_ranked_db, rows, blocked_db, blocked_param_indices, 
-%			  block_levels, crit_db)
+% [a_db, ranked_dbs] = 
+%   blockedDistances(a_ranked_db, rows, blocked_db, blocked_param_indices, 
+%	  	     block_levels, crit_db)
 %
 % Description:
 %
@@ -19,6 +21,7 @@ function a_db = blockedDistances(a_ranked_db, rows, blocked_db, blocked_param_in
 %		
 %   Returns:
 %	a_db: A tests_db object with the matrix of distances.
+%	ranked_dbs: A cell array of ranked_dbs for each row.
 %
 %   Example:
 %	>> dist_matx_db = blockedDistances(rankMatching(super_db, matchingRow(rsuper_phys_db, 20)), 1:5, super_blocker_db, [1 2], 10, matchingRow(rsuper_phys_db, 21))
@@ -38,39 +41,57 @@ end
 joined_db = joinOriginal(a_ranked_db);
 
 data = repmat(NaN, length(rows), block_levels + 1);
+row_names = cell(1, length(rows));
+ranked_dbs = cell(1, length(rows));
 
+%# For each of the rows from a_ranked_db
 out_row = 1;
 for row_num=rows
   %# Get blocked parameter scan
   blocked_rows_db = getBlockedParamRows(joined_db, row_num, blocked_param_indices, ...
-					block_levels)
-
-  displayRow(blocked_rows_db)
+					block_levels);
+  %#displayRow(blocked_rows_db)
 
   %# Find matching indices
-  row_indices = getParamRowIndices(blocked_rows_db, ':', blocked_db)
+  row_indices = getParamRowIndices(blocked_rows_db, ':', blocked_db);
 
   %# Get matching rows in a new db
   this_blocked = onlyRowsTests(blocked_db, row_indices(~isnan(row_indices)), ':');
 
   %# rank it
-  this_ranked = rankMatching(this_blocked, crit_db);
+  this_ranked = rankMatching(this_blocked, crit_db); %#, a_ranked_db.props
 
   %# reorder with RowIndex
   this_ranked = sortrows(this_ranked, 'RowIndex');
+  ranked_dbs{out_row} = this_ranked;
 
   %# Get the distances
   this_dists = onlyRowsTests(this_ranked, ':', 'Distance');
+  %#displayRow(this_dists)
 
-  size(data(out_row, ~isnan(row_indices)))
-  dbsize(this_dists)
+  %#size(data(out_row, ~isnan(row_indices)))
+  %#dbsize(this_dists)
 
   data(out_row, ~isnan(row_indices)) = get(this_dists, 'data')';
+
+  %# Decoration
+  row_names{out_row} = [ 'Rank_' num2str(row_num) ];
+
   out_row = out_row + 1;
 end
 
 %# Test names not necessary?
 %#names(1:block_levels) = num2cell(1:block_levels);
 
-a_db = tests_db(data, {}, {}, ['Distance matrix for blockers on rankings in ' ...
-			       get(a_ranked_db, 'id') ] );
+%# Column names
+col_names = cell(1, block_levels + 1);
+orig_names = fieldnames(joined_db.col_idx);
+for block=0:block_levels
+  col_names{block + 1} = [ sprintf('%s_%d_Percent', ...
+				   orig_names{blocked_param_indices(1)}, ...
+				   block * 100 / block_levels)  ];
+end
+
+a_db = tests_db(data, col_names, row_names, ...
+		['Distance matrix for blockers of rankings in ' ...
+		 get(a_ranked_db, 'id') ] );
