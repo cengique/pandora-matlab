9246b320390593d200db94f1308dee912d07ad09
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-05-13 22:37:27 +0000

    Generic set and setProp convenience methods.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@217 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @physiol_cip_traceset_fileset/set.m
@@ -0,0 +1,8 @@
+function a = set(a, attr, val)
+% set - Generic method for setting object attributes.
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
+try
+  a.(attr) = val;
+catch
+  a.params_tests_dataset = set(a.params_tests_dataset, attr, val);
+end
