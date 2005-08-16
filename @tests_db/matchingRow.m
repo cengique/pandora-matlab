b5335eeac590141cadcd30c39ea89efb7d8cd890
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-08-16 20:20:30 +0000

    Added optional param for external db to take the STDs from.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@260 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/matchingRow.m
+++ @tests_db/matchingRow.m
@@ -1,9 +1,9 @@
-function crit_db = matchingRow(db, row, tests)
+function crit_db = matchingRow(db, row, props)
 
 % matchingRow - Creates a criterion database for matching the tests of a row.
 %
 % Usage:
-% crit_db = matchingRow(db, row, tests)
+% crit_db = matchingRow(db, row, tests, props)
 %
 % Description:
 %   Copies selected test values from row as the first row into the 
@@ -12,7 +12,8 @@ function crit_db = matchingRow(db, row, tests)
 %   Parameters:
 %	db: A tests_db object.
 %	row: A row index to match.
-%	tests: Test name of column indices (see tests2cols).
+%	props: A structure with any optional properties.
+%		std_db: Take the standard deviation from this db instead.
 %		
 %   Returns:
 %	crit_db: A tests_db with two rows for values and STDs.
@@ -22,15 +23,24 @@ function crit_db = matchingRow(db, row, tests)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/12/08
 
-if ~ exist('tests')
-  tests = ':';
+if ~ exist('props')
+  props = struct;
 end
+
+tests = ':';
 
 cols = tests2cols(db, tests);
 col_db = onlyRowsTests(db, row, tests);
 
+%# Check for std_db
+if isfield(props, 'std_db')
+  std_db = props.std_db;
+else
+  std_db = db;
+end
+
 %# Add the row index
-data = [ col_db.data, row; std(db(:, tests)), 0 ];
+data = [ col_db.data, row; std(std_db(:, tests)), 0 ];
 
 %# Create the criterion database
 col_name_cell = fieldnames(db.col_idx);
