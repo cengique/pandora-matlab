e73ff94300739539544a740b0809d93304ec36cb
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-08-16 20:15:58 +0000

    Fixed issue with tiny plots by putting minimal frame size.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@255 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @plot_abstract/plot.m
+++ @plot_abstract/plot.m
@@ -21,6 +21,8 @@ function handles = plot(a_plot, layout_axis)
 
 %# Fixed size for ticks and labels
 decosize = 0.04;
+minwidth = 0.01;
+minheight = 0.01;
 
 %# TODO: save the axis handle!
 if exist('layout_axis')
@@ -37,25 +39,25 @@ a_plot_props = get(a_plot, 'props');
 if ~isfield(a_plot_props, 'noXLabel') || a_plot_props.noXLabel == 0
   y_strut = decosize / 2;
   bottom_side = bottom_side + y_strut;
-  height = height - y_strut;
+  height = max(height - y_strut, minheight);
 end
 
 if ~isfield(a_plot_props, 'XTickLabel') || ~isempty(a_plot_props.XTickLabel)
   y_strut = decosize;
   bottom_side = bottom_side + y_strut;
-  height = height - y_strut;
+  height = max(height - y_strut, minheight);
 end
 
 if ~isfield(a_plot_props, 'noYLabel') || a_plot_props.noYLabel == 0
   x_strut = decosize / 2;
   left_side = left_side + x_strut;
-  width = width - x_strut;
+  width = max(width - x_strut, minwidth);
 end
 
 if ~isfield(a_plot_props, 'YTickLabel') || ~isempty(a_plot_props.YTickLabel)
   x_strut = decosize;
   left_side = left_side + x_strut;
-  width = width - x_strut;
+  width = max(width - x_strut, minwidth);
 end
 
 axes('position', [left_side bottom_side width height]);
