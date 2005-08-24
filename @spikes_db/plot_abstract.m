f056d3a578bd0ac6f180e277540aa4e4c75be0d6
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-08-24 17:38:51 +0000

    Custom spike database class with its own visualization routine.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@275 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @spikes_db/plot_abstract.m
@@ -0,0 +1,42 @@
+function a_pm = plot_abstract(a_db, title_str)
+
+% plot_abstract - Visualizes the spikes_db by marking spike shapes measurements on the trace plot.
+%
+% Usage:
+% a_pm = plot_abstract(a_db, title_str)
+%
+% Description:
+%
+%   Parameters:
+%	a_db: A spikes_db object.
+%	title_str: (Optional) A string to be concatanated to the title.
+%		
+%   Returns:
+%	a_pm: A trace plot.
+%
+% Example: (see tests_db/plot_abstract)
+%
+% See also: plot_abstract/plot_abstract, tests_db/plot_abstract, plotFigure
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2005/08/17
+
+if ~ exist('title_str')
+  title_str = '';
+end
+
+%# first plot the trace
+trace_plot = plotData(a_db.trace);
+
+num_spikes = dbsize(a_db, 1);
+
+%# Then superpose all spike measures
+[plots(1:num_spikes)] = deal(plot_abstract);
+for spike_num=1:num_spikes
+  ssp = spike_shape_profile(rows2Struct(a_db, spike_num), spike_shape);
+  results = getResults(ssp);
+  plots(spike_num) = plot_abstract(ssp, struct('absolute_peak_time', results.Time, ...
+					       'no_plot_spike', 1));
+end
+a_pm = superposePlots([trace_plot, plots]);
+
---
+++ @spikes_db/spikes_db.m
@@ -0,0 +1,55 @@
+function a_spikes_db = spikes_db(data, col_names, a_trace, a_period, id, props)
+
+% spikes_db - A database of spike shape results obtained from a period in a trace.
+%
+% Usage:
+% a_spikes_db = spikes_db(data, col_names, a_trace, a_period, id, props)
+%
+% Description:
+%   This is a subclass of tests_db. Use one of the clustering methods in 
+% tests_db, such as kmeansCluster, to get an instance of this class.
+%
+%   Parameters:
+%	data: Database contents.
+%	col_names: The column names.
+%	a_trace: The trace where the spikes were found.
+%	a_period: The period inside a_trace where spikes were found.
+%	id: An identifying string.
+%	props: A structure with any optional properties.
+%		
+%   Returns a structure object with the following fields:
+%	tests_db, trace, period, props.
+%
+% General operations on spikes_db objects:
+%   spikes_db		- Construct a new spikes_db object.
+%   plot_abstract	- Creates a trace plot with spike shapes annotated in red.
+%
+% Additional methods:
+%	See methods('spikes_db')
+%
+% See also: tests_db, trace, period
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2005/08/17
+
+if nargin == 0 %# Called with no params
+   a_spikes_db.trace = trace;
+   a_spikes_db.period = period;
+   a_spikes_db.props = struct([]);
+   a_spikes_db = class(a_spikes_db, 'spikes_db', tests_db);
+ elseif isa(data, 'spikes_db') %# copy constructor?
+   a_spikes_db = data;
+ else %# Create a new object
+
+   if ~ exist('props')
+     props = struct([]);
+   end
+
+   a_spikes_db.trace = a_trace;
+   a_spikes_db.period = a_period;
+   a_spikes_db.props = props;
+
+   %# Create subclass object
+   a_spikes_db = class(a_spikes_db, 'spikes_db', ...
+		       tests_db(data, col_names, {}, id, props));
+end
