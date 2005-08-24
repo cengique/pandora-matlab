2ee521313ba63d1037b102ce111e8108c90c7c6e
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-08-24 17:39:47 +0000

    Added default way of visualization by plotting test histograms.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@276 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @tests_db/plot_abstract.m
@@ -0,0 +1,33 @@
+function a_pm = plot_abstract(a_db, title_str)
+
+% plot_abstract - Default visualization for a database.
+%
+% Usage:
+% a_pm = plot_abstract(a_db, title_str)
+%
+% Description:
+%   Calls plotTestsHistsMatrix. Subclasses should override this method
+% to provide their own visualization.
+%
+%   Parameters:
+%	a_db: A params_tests_db object.
+%	title_str: (Optional) A string to be concatanated to the title.
+%		
+%   Returns:
+%	a_pm: A plot_stack with the plots organized in matrix form
+%
+% Example:
+%   >> plot(my_db, ': first impression')
+% will call this function and send the generated plot to the plotFigure function.
+%
+% See also: plot_abstract/plot_abstract, plotTestsHistsMatrix, plotFigure
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2005/08/17
+
+if ~ exist('title_str')
+  title_str = '';
+end
+
+a_pm = plotTestsHistsMatrix(a_db, title_str);
+
