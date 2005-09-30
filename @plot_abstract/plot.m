4188982c9d0ae6d3b77ab73b962f71d1f701481b
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-09-30 20:52:19 +0000

    Made layout_axis to have NaN value to not open new axis. Added new layout algorithm for more consistent operation. Added LineStyleOrder optional property.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@301 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @plot_abstract/plot.m
+++ @plot_abstract/plot.m
@@ -10,6 +10,7 @@ function handles = plot(a_plot, layout_axis)
 %   Parameters:
 %	a_plot: A plot_abstract object, or a subclass object.
 %	layout_axis: The axis position to layout this plot (Optional). 
+%		     If NaN, doesn't open a new axis.
 %		
 %   Returns:
 %	handles: Handles of graphical objects drawn.
@@ -25,7 +26,7 @@ minwidth = 0.01;
 minheight = 0.01;
 
 %# TODO: save the axis handle!
-if exist('layout_axis')
+if exist('layout_axis') == 1 && ~all(isnan(layout_axis))
   left_side = layout_axis(1);
   bottom_side = layout_axis(2);
   width = layout_axis(3);
@@ -34,33 +35,43 @@ else
   [ left_side bottom_side width height ] = deal(0, 0, 1, 1);
 end
 
-a_plot_props = get(a_plot, 'props');
-%# Adjust the axis according to decorations
-if ~isfield(a_plot_props, 'noXLabel') || a_plot_props.noXLabel == 0
-  y_strut = decosize / 2;
-  bottom_side = bottom_side + y_strut;
-  height = max(height - y_strut, minheight);
+if ~isnan(layout_axis)
+  a_plot_props = get(a_plot, 'props');
+  plot_axis_labels = get(a_plot, 'axis_labels');
+  %# Adjust the axis according to decorations
+  if (~isempty(plot_axis_labels) && ~isempty(plot_axis_labels{1})) && ...
+	(~isfield(a_plot_props, 'noXLabel') || a_plot_props.noXLabel == 0)
+    y_strut = decosize / 2;
+    bottom_side = bottom_side + y_strut;
+    height = max(height - y_strut, minheight);
+  end
+  
+  if ~isfield(a_plot_props, 'XTickLabel') || ~isempty(a_plot_props.XTickLabel)
+    y_strut = decosize;
+    bottom_side = bottom_side + y_strut;
+    height = max(height - y_strut, minheight);
+  end
+
+  if (~isempty(plot_axis_labels) && ~isempty(plot_axis_labels{2})) && ...
+	(~isfield(a_plot_props, 'noYLabel') || a_plot_props.noYLabel == 0)
+    x_strut = decosize / 2;
+    left_side = left_side + x_strut;
+    width = max(width - x_strut, minwidth);
+  end
+  
+  if ~isfield(a_plot_props, 'YTickLabel') || ~isempty(a_plot_props.YTickLabel)
+    x_strut = decosize;
+    left_side = left_side + x_strut;
+    width = max(width - x_strut, minwidth);
+  end
+
+  axes('position', [left_side bottom_side width height]);
 end
 
-if ~isfield(a_plot_props, 'XTickLabel') || ~isempty(a_plot_props.XTickLabel)
-  y_strut = decosize;
-  bottom_side = bottom_side + y_strut;
-  height = max(height - y_strut, minheight);
+if isfield(a_plot_props, 'LineStyleOrder')
+  set(gca, 'LineStyleOrder', {a_plot_props.LineStyleOrder});
+  set(gca, 'NextPlot', 'replacechildren');
 end
-
-if ~isfield(a_plot_props, 'noYLabel') || a_plot_props.noYLabel == 0
-  x_strut = decosize / 2;
-  left_side = left_side + x_strut;
-  width = max(width - x_strut, minwidth);
-end
-
-if ~isfield(a_plot_props, 'YTickLabel') || ~isempty(a_plot_props.YTickLabel)
-  x_strut = decosize;
-  left_side = left_side + x_strut;
-  width = max(width - x_strut, minwidth);
-end
-
-axes('position', [left_side bottom_side width height]);
 
 %# Run the plot command
 if strcmp(a_plot.command, 'boxplot') || strcmp(a_plot.command, 'boxplotp')
