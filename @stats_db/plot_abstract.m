6aca1e9f7a85a423d9ff45d7a426ac930bbbfd81
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-09-30 20:57:47 +0000

    Minor fixes.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@307 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @stats_db/plot_abstract.m
+++ @stats_db/plot_abstract.m
@@ -1,18 +1,17 @@
-function a_plot = plot_abstract(a_stats_db, props)
+function a_plot = plot_abstract(a_stats_db, title_str, props)
 
-% plot_abstract - Generates an error bar representation for each of the columns
-%	in this db. Looks for 'min', 'max', and 'std' labels in the row_idx
-%	for drawing the errorbars.
+% plot_abstract - Generates an error bar graph for each db columns. Looks for 'min', 'max', and 'std' labels in the row_idx for drawing the errorbars.
 %
 % Usage:
-% a_plot = plot_abstract(a_stats_db)
+% a_plot = plot_abstract(a_stats_db, title_str, props)
 %
 % Description:
 %   Generates a plot_simple object from this histogram.
 %
 %   Parameters:
 %	a_stats_db: A histogram_db object.
-%	command: Plot command (Optional, default='bar')
+%	title_str: A title string on the plot
+%	props: A structure with any optional properties.
 %		
 %   Returns:
 %	a_plot: A object of plot_abstract or one of its subclasses.
@@ -22,8 +21,8 @@ function a_plot = plot_abstract(a_stats_db, props)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
 
-if ~ exist('command')
-  command = 'bar';
+if ~ exist('title_str')
+  title_str = '';
 end
 
 if ~ exist('props')
@@ -49,12 +48,14 @@ if isfield(row_idx, 'max')
   highs = data(row_idx.max,:) - data(1,:);
 end
 
-if isfield(a_stats_db.props, 'axis_limits')
-  axis_limits = a_stats_db.props.axis_limits;
+stats_props = get(a_stats_db, 'props');
+if isfield(stats_props, 'axis_limits')
+  axis_limits = stats_props.axis_limits;
 else
   axis_limits = [];
 end
 
 a_plot = plot_errorbars(data(1,:), lows, highs, ...
-		       col_names, get(a_stats_db, 'id'), axis_limits, ...
-		       mergeStructs(props, a_stats_db.props));
+			col_names, [ get(a_stats_db, 'id') title_str], ...
+			axis_limits, ...
+			mergeStructs(props, stats_props));
