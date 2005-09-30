f71f2fac796133ab856556fa5028c7244ea7470e
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-09-30 21:02:09 +0000

    Extended comparison to handle multiple rows. This allows computations similar to nested SQL queries.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@310 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/compareRows.m
+++ @tests_db/compareRows.m
@@ -1,18 +1,19 @@
-function compared = compareRows(db, row)
+function [rows, compared] = compareRows(db, row)
 
-% compareRows - Returns comparison results of db and the given row
-%		in a column vector.
+% compareRows - Returns comparison results of db and the given row in a column vector.
 %
 % Usage:
 % rows = compareRows(db, row)
 %
 % Description:
-% Uses summation of distance for magnitude comparison. That is, all columns
-% have the same weight.
+% If the row argument has multiple rows, the comparison is done separately
+% for each of its rows and the results are the logical OR of those.
+% Note that, it uses summation of distance for magnitude comparison. 
+% That is, all columns have the same weight.
 %
 %   Parameters:
 %	db: A tests_db object.
-%	row: Row array to be compared with db rows.
+%	row: Row array, matrix or database to be compared with db rows.
 %		
 %   Returns:
 %	rows: A column vector of comparison results. 
@@ -23,17 +24,29 @@ function compared = compareRows(db, row)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/17
 
+%# Compare two dbs?
+if isa(row, 'tests_db')
+  row = row.data;
+end
+
 if dbsize(db, 2) ~= size(row, 2)
   error('Row must contain same columns as the db.');
 end
 
-%# Find doesn't work in two dimension comparisons
-%# Thus, use algorithm:
+%# If multiple rows in row
+if size(row, 1) > 1
+  %# recurse
+  rows = compareRows(db, row(1, 1)) & compareRows(db, row(2:end, 1));
+  compared = NaN;
+else
+  %# Find doesn't work in two dimension comparisons
+  %# Thus, use algorithm:
 
-%# - duplicate row to a matrix of same size with db
-row_matx = ones(dbsize(db, 1), 1) * row;
+  %# - duplicate row to a matrix of same size with db
+  row_matx = ones(dbsize(db, 1), 1) * row;
 
-%# - subtract from db
-compared = db.data - row_matx;
+  %# - subtract from db
+  compared = db.data - row_matx;
 
-rows = sum(compared, 2) ~= 0;
+  rows = sum(compared, 2) ~= 0;
+end
