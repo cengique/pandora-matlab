6ad0feee766aad8964debad3769031e669bbfe84
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-10-16 23:01:53 +0000

    overloaded functions for adding/removing columns from DB.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@320 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @params_tests_db/addParams.m
@@ -0,0 +1,52 @@
+function obj = addParams(obj, param_names, param_columns)
+
+% addParams - Inserts new parameter columns to tests_db.
+%
+% Usage:
+% obj = addParams(obj, param_names, param_columns)
+%
+% Description:
+%   Adds new columns to the database and returns the new DB.
+%   This operation is expensive in the sense that the whole database matrix
+%   needs to be enlarged just to add a 
+%   single new column. The method of allocating a matrix, filling it up, and
+%   then providing it to the tests_db constructor is the preferred method 
+%   of creating tests_db objects. This method may be used for 
+%   measures obtained by operating on raw measures.
+%
+%   Parameters:
+%	obj: A tests_db object.
+%	param_names: A cell array of param names to be added.
+%	param_columns: Data matrix of columns to be added.
+%		
+%   Returns:
+%	obj: The tests_db object that includes the new columns.
+%
+% See also: allocateRows, tests_db
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2005/10/11
+
+if (dbsize(obj, 1) > 0 && size(param_columns, 1) ~= dbsize(obj, 1))
+  error(['Number of rows in column (' num2str(size(param_columns, 1)) ') ', ...
+	 'does not match rows in DB (' num2str(dbsize(obj, 1)) ').']);
+end
+
+if length(param_names) ~= size(param_columns, 2)
+  error(['Number of parameter names (' num2str(length(param_names)) ') ', ...
+	 'does not match columns in matrix (' num2str(size(param_columns, 2)) ').']);
+end
+
+num_params = get(obj, 'num_params');
+
+%# Add the column to the parameter DB
+a_param_db = addColumns(onlyRowsTests(obj, ':', 1:num_params), ...
+			param_names, param_columns);
+
+%# Concat the rest of original DB 
+a_test_db = onlyRowsTests(obj, ':', (num_params + 1):dbsize(obj, 2));
+obj = addColumns(a_param_db, fieldnames(get(a_test_db, 'col_idx')), ...
+		 get(a_test_db, 'data'));
+
+%# Adjust number of params
+obj.num_params = num_params + length(param_names);
---
+++ @params_tests_db/delColumns.m
@@ -0,0 +1,26 @@
+function obj = delColumns(obj, tests)
+
+% delColumns - Deletes columns from tests_db.
+%
+% Usage:
+% index = delColumns(obj, tests)
+%
+% Description:
+%   Overloaded function that maintains correct number of parameters. See
+% original tests_db/delColumns.
+%
+%   Parameters:
+%	obj: A tests_db object.
+%	tests: Numbers or names of tests (see tests2cols)
+%		
+%   Returns:
+%	obj: The tests_db object that is missing the columns.
+%
+% See also: tests_db/delColumns
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2005/10/11
+
+cols = sort(tests2cols(obj, tests));
+obj.tests_db = delColumns(obj.tests_db, tests);
+obj.num_params = obj.num_params - sum(cols <= obj.num_params);
