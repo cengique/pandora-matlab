302b889987884041b42b014b9d654901316b3996
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-10-16 23:10:51 +0000

    Also filters out inf values.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@335 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/noNaNRows.m
+++ @tests_db/noNaNRows.m
@@ -1,6 +1,6 @@
 function a_db = noNaNRows(a_db)
 
-% noNaNRows - Returns a DB by removing any NaN containing rows.
+% noNaNRows - Returns a DB by removing any NaN or Inf containing rows.
 %
 % Usage:
 % a_db = noNaNRows(a_db)
@@ -18,4 +18,4 @@ function a_db = noNaNRows(a_db)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2005/09/21
 
-a_db = onlyRowsTests(a_db, ~ isnanrows(a_db), ':');
+a_db = onlyRowsTests(a_db, ~ isnanrows(a_db) & ~ isinf(a_db), ':');
