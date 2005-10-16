40c29b29fc907ad7cb795eae88d653acfb5c8244
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-10-16 23:09:40 +0000

    Utility to delete some columns.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@332 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @tests_db/delColumns.m
@@ -0,0 +1,45 @@
+function obj = delColumns(obj, tests)
+
+% delColumns - Deletes columns from tests_db.
+%
+% Usage:
+% index = delColumns(obj, tests)
+%
+% Description:
+%   Deletes test columns from the database and returns the new DB.
+%   This operation is expensive in the sense that the whole database matrix
+%   needs to be copied just to delete a 
+%   single column. The method of allocating a matrix, filling it up, and
+%   then providing it to the tests_db constructor is the preferred method 
+%   of creating tests_db objects. This method may be used for 
+%   measures obtained by operating on raw measures.
+%
+%   Parameters:
+%	obj: A tests_db object.
+%	tests: Numbers or names of tests (see tests2cols)
+%		
+%   Returns:
+%	obj: The tests_db object that is missing the columns.
+%
+% See also: allocateRows, tests_db
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2005/10/06
+
+cols = tests2cols(obj, tests);
+mask = true(1, dbsize(obj, 2));
+
+%# delete the columns
+obj.data(:, cols) = [];
+
+%# Update the meta-data
+col_idx = get(obj, 'col_idx');
+test_names = fieldnames(col_idx);
+
+%# remove the names
+mask(cols) = false;
+test_names = {test_names{mask}};
+
+%# Make new col_idx
+new_col_idx = makeIdx(test_names);
+obj = set(obj, 'col_idx', new_col_idx);
