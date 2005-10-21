76ee65648c6460be8f33ea6f3f89b2de784c3461
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-10-21 19:13:12 +0000

    Jeremy added minamp param.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@348 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @trace/spikes.m
+++ @trace/spikes.m
@@ -1,16 +1,19 @@
-function obj = spikes(t, a_period, plotit)
+function obj = spikes(t, a_period, plotit, minamp)
 
 % spikes - Convert trace to spikes object for spike timing calculations.
 %
 % Usage:
-% obj = spikes(trace, a_period, plotit)
+% obj = spikes(trace, a_period, plotit, minamp)
 %
 %   Parameters:
 %	trace: A trace object.
 %	a_period: A period object denoting the part of trace of interest 
-%		(optional).
+%		(optional, if empty vector, taken as wholePeriod).
 %	plotit: If non-zero, a plot is generated for showing spikes found
 %		(optional).
+%	minamp: minimum amplitude that must be reached if using findFilteredSpikes.
+%		--> adjust as needed to discriminate spikes from EPSPs.
+%		(optional)
 %
 % Description:
 %   Creates a spikes object.
@@ -20,15 +23,23 @@ function obj = spikes(t, a_period, plotit)
 % $Id$
 % Author: 
 %   Cengiz Gunay <cgunay@emory.edu>, 2004/07/30
+% Modified:
+% - added minamp parameter. J. Edgerton, 10/10/2005
 
 if nargin == 0 %# Called with no params
   error('Need trace parameter.');
-elseif nargin == 1 
-  a_period = periodWhole(t);
 end
 
+if ~ exist('a_period') || isempty(a_period)
+  a_period = periodWhole(t);
+end
+  
 if ~ exist('plotit')
   plotit = 0;
+end
+
+if ~ exist('minamp')
+  minamp = 10;	% default.
 end
 
 if (a_period.end_time - a_period.start_time) > 0
@@ -44,8 +55,8 @@ if isfield(t.props, 'spike_finder') & ...
       findspikes(t.data(a_period.start_time:a_period.end_time) * mV_factor, ...
 		 t.props.threshold, plotit);
 else %# Assume spike_finder == 1 for filtered method
-  [times, peaks, n] = ...
-      findFilteredSpikes(t, a_period, plotit);
+	[times, peaks, n] = ...
+      	findFilteredSpikes(t, a_period, plotit, minamp);
 end
 
 obj = spikes(times, length(t.data), t.dt, t.id);
