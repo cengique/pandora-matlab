7890843841bed05f9cf1881b063f58d688e0e8b4
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-11-22 17:41:21 +0000

    added the tracesetindex as the name, to be clear on plot titles.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@363 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @physiol_cip_traceset/cip_trace.m
+++ @physiol_cip_traceset/cip_trace.m
@@ -1,15 +1,16 @@
-function a_cip_trace = cip_trace(traceset, trace_index)
+function a_cip_trace = cip_trace(traceset, trace_index, props)
 
 % cip_trace - Loads a cip_trace object from a raw data file in the traceset.
 %
 % Usage:
-% a_cip_trace = cip_trace(traceset, trace_index)
+% a_cip_trace = cip_trace(traceset, trace_index, props)
 %
 % Description:
 %
 %   Parameters:
 %	traceset: A physiol_cip_traceset object.
 %	trace_index: Index of file in traceset.
+%	props: A structure with any optional properties.
 %		
 %   Returns:
 %	a_cip_trace: A cip_trace object that holds the raw data.
@@ -19,18 +20,28 @@ function a_cip_trace = cip_trace(traceset, trace_index)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2005/07/13
 
+if ~ exist('props')
+  props = struct;
+end
+
 %# First call CIPform and get necessary parameters
 [type, on, off, finish, bias, pulse] = CIPform(traceset, trace_index);
 pulse_time_start=on;
 pulse_time_width= off - on + 1;
 ygain = 1 / traceset.vgain;
-props=struct('type', type, 'on', on, 'off', off, 'finish', finish, 'bias', ...
-	     bias, 'pulse', pulse, 'channel', traceset.vchan, 'scale_y', ...
+new_props=struct('type', type, 'on', on, 'off', off, 'finish', finish, 'bias', ...
+		 bias, 'pulse', pulse, 'channel', traceset.vchan, 'scale_y', ...
 		 ygain, 'traces', num2str(getItem(traceset, trace_index)));
+
+if isfield(props, 'TracesetIndex')
+  trace_id = [ 's' num2str(props.TracesetIndex) ',t' num2str(trace_index) ];
+else
+  trace_id = [ 't' num2str(trace_index) ];
+end
 
 traceset_props = get(traceset, 'props');
 a_cip_trace = cip_trace(traceset.data_src, get(traceset, 'dt'), ...
 			get(traceset, 'dy'), ...
 			pulse_time_start, pulse_time_width, ...
-			[get(traceset, 'id') '(' num2str(trace_index) ')'], ...
-			mergeStructs(traceset_props, props));
+			[get(traceset, 'id') '(' trace_id ')'], ...
+			mergeStructs(traceset_props, new_props));
