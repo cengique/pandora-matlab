3aa352385bd62e0e87e5c054c57a0e414bcd82b7
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-11-22 17:53:07 +0000

    work on original DB object, rather than creating a new one. (for preserving params_tests_db objs)
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@382 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/matchingRow.m
+++ @tests_db/matchingRow.m
@@ -3,7 +3,7 @@ function crit_db = matchingRow(db, row, props)
 % matchingRow - Creates a criterion database for matching the tests of a row.
 %
 % Usage:
-% crit_db = matchingRow(db, row, tests, props)
+% crit_db = matchingRow(db, row, props)
 %
 % Description:
 %   Copies selected test values from row as the first row into the 
@@ -17,6 +17,10 @@ function crit_db = matchingRow(db, row, props)
 %		
 %   Returns:
 %	crit_db: A tests_db with two rows for values and STDs.
+%
+%   Example:
+%	>> crit_db = matchingRow(phys_control_compare_db, 
+%		find(phys_control_compare_db(:, 'TracesetIndex') == 61))
 %
 % See also: rankMatching, tests_db, tests2cols
 %
@@ -40,13 +44,12 @@ else
 end
 
 %# Add the row index
-data = [ col_db.data, row; std(std_db(:, tests)), 0 ];
+col_added_db = addColumns(col_db, {'RowIndex'}, row);
 
-%# Create the criterion database
-col_name_cell = fieldnames(db.col_idx);
-col_names = col_name_cell(cols);
+%# Add the STD row
+std_db = addLastRow(col_added_db, [ std(std_db(:, tests)), 0 ]);
 
-%# TODO: put the invarName in the title?
-crit_db = tests_db(data, {col_names{:}, 'RowIndex'}, {'Values', 'STD'}, ...
-		   ['Criterion for matching row ' num2str(row) ...
-		    ' from ' db.id ], db.props);
+%# Adjust other fields
+crit_db = set(std_db, 'row_idx', makeIdx({'Values', 'STD'}));
+crit_db = set(crit_db, 'id', ['Criterion for matching row ' num2str(row) ...
+			      ' from ' db.id ]);
