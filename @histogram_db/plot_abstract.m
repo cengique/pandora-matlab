ced82005c8ce3d2493dc6143e852fb361ff023c4
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-11-22 17:34:57 +0000

    Improved histogram plotting.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@355 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @histogram_db/plot_abstract.m
+++ @histogram_db/plot_abstract.m
@@ -1,17 +1,18 @@
-function a_plot = plot_abstract(a_hist_db, command, props)
+function a_plot = plot_abstract(a_hist_db, title_str, props)
 
 % plot_abstract - Generates a plottable description of this object.
 %
 % Usage:
-% a_plot = plot_abstract(a_hist_db, command, props)
+% a_plot = plot_abstract(a_hist_db, title_str, props)
 %
 % Description:
 %   Generates a plot_simple object from this histogram.
 %
 %   Parameters:
 %	a_hist_db: A histogram_db object.
-%	command: Plot command (Optional, default='bar')
 %	props: Optional properties passed to plot_abstract.
+%		command: Plot command (Optional, default='bar')
+%		quiet: If 1, don't include database name on title.
 %		
 %   Returns:
 %	a_plot: A object of plot_abstract or one of its subclasses.
@@ -29,13 +30,17 @@ if ~ exist('props')
   props = struct([]);
 end
 
+if ~ exist('title_str')
+  title_str = '';
+end
+
 %# If input is an array, then return array of plots
 num_dbs = length(a_hist_db);
 if num_dbs > 1 
   %# Create array of plots
   [a_plot(1:num_dbs)] = deal(plot_simple);
   for plot_num = 1:num_dbs
-    a_plot(plot_num) = plot_abstract(a_hist_db(plot_num), command, props);
+    a_plot(plot_num) = plot_abstract(a_hist_db(plot_num), title_str, props);
   end
   return;
 end
@@ -48,14 +53,20 @@ data = get(a_hist_db, 'data');
 %# if the plot is rotated switch the axis labels
 if strcmp(command, 'barh')
   x_label = 'Count';
-  y_label = colnames{1};
+  y_label = strrep(colnames{1}, '_', ' ');
 else
-  x_label = colnames{1};
+  x_label = strrep(colnames{1}, '_', ' ');
   y_label = 'Count';
+end
+
+if ~ isfield(props, 'quiet')
+  all_title = [ 'Histogram of ' strrep(get(a_hist_db, 'id'), '_', '\_') title_str ];
+else
+  all_title = title_str;
 end
 
 %# Make a simple plot object drawing vertical bars
 a_plot = plot_simple(data(:, 1), data(:, 2), ...
-		     [ 'Histogram of ' strrep(get(a_hist_db, 'id'), '_', '\_') ], ...
+		     all_title, ...
 		     x_label, y_label, ...
 		     colnames{1}, command, props);
