7946dd9714fdeaa5ce7f90a3b931656bfe9d0c34
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-11-22 17:47:41 +0000

    added exception for not accepting RHS invocations.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@372 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @plot_superpose/set.m
+++ @plot_superpose/set.m
@@ -1,11 +1,15 @@
 function b = set(a, attr, val)
 % set - Generic method for setting object attributes.
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
-try
-  eval(['a.' attr ' = val;']);
-  b = a;
-catch
-  errstr = lasterror;
-  a.plot_abstract = set(a.plot_abstract, attr, val);
-  b = a;
+if ~ isa(a, 'plot_abstract')
+  builtin('set', a, attr, val);
+else
+  try
+    a.(attr) = val;
+    b = a;
+  catch
+    errstr = lasterror;
+    a.plot_abstract = set(a.plot_abstract, attr, val);
+    b = a;
+  end
 end
