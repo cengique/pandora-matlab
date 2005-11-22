b5eb8e8fb2f725ed960464d9d8b368b0d274c11f
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-11-22 17:36:47 +0000

    Read parameter names and values from param file, like loadItemProfile.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@356 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_cip_trace_fileset/cip_trace.m
+++ @params_cip_trace_fileset/cip_trace.m
@@ -33,10 +33,33 @@ else
   filename = getItem(fileset, file_index);
   fullname = fullfile(get(fileset, 'path'), filename);
 
+  props = get(fileset, 'props');
+  if isfield(props, 'param_rows')
+    %# Take parameter values from the specified parameter file,
+    %# in addition to the ones specified on data filenames.
+    names_vals = parseGenesisFilename(fullname);
+    if isfield(props, 'num_params')
+      num_params = props.num_params;
+    else
+      num_params = size(names_vals, 1);
+    end
+    str_index = strmatch('trial', names_vals{1:num_params, 1});
+    
+    if length(str_index) < 1
+      error(['Parameter lookup from rows is requested, but cannot find ' ...
+	     'the "trial" parameter in the data filename ' fullname ]);
+    end
+    
+    trial_num = names_vals{str_index, 2};
+    trace_id = [ 't' num2str(trial_num) ];
+  else
+    trace_id = num2str(file_index);
+  end
+
   %# Load a cip_trace object
   a_cip_trace = ...
       cip_trace(fullname, get(fileset, 'dt'), get(fileset, 'dy'), ...
 		fileset.pulse_time_start, fileset.pulse_time_width, ...
-		[get(fileset, 'id') '(' num2str(file_index) ')'], ...
+		[get(fileset, 'id') '(' trace_id ')'], ...
 		get(fileset, 'props'));
 end
