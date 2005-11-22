711bdca1930c7f70522ac0b4f9c5caebbacac773
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-11-22 17:54:16 +0000

    More controls and beautified display.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@385 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/plotTestsHistsMatrix.m
+++ @tests_db/plotTestsHistsMatrix.m
@@ -1,9 +1,9 @@
-function a_pm = plotTestsHistsMatrix(a_db, title_str)
+function a_pm = plotTestsHistsMatrix(a_db, title_str, props)
 
 % plotTestsHistsMatrix - Create a matrix plot of test histograms.
 %
 % Usage:
-% a_pm = plotTestsHistsMatrix(a_db, title_str)
+% a_pm = plotTestsHistsMatrix(a_db, title_str, props)
 %
 % Description:
 %   Skips the 'ItemIndex' test.
@@ -11,7 +11,11 @@ function a_pm = plotTestsHistsMatrix(a_db, title_str)
 %   Parameters:
 %	a_db: A params_tests_db object.
 %	title_str: (Optional) A string to be concatanated to the title.
-%		
+%	props: A structure with any optional properties, passed to plot_abstract.
+%	  orient: Orientation of the plot_stack. 'x', 'y', or 'matrix' (default).
+%	  histBins: Number of histogram bins.
+%	  quiet: Don't put the DB id on the title.
+% 
 %   Returns:
 %	a_pm: A plot_stack with the plots organized in matrix form
 %
@@ -24,7 +28,36 @@ if ~ exist('title_str')
   title_str = '';
 end
 
-a_pm = matrixPlots(plot_abstract(testsHists(a_db), 'bar', ...
-				 struct('rotateXLabel', 20)), ...
-		   {}, ['Measure histograms for ' get(a_db, 'id') title_str ]);
+if ~ exist('props')
+  props = struct;
+end
+
+if isfield(props, 'histBins')
+  hist_pars = {a_db, props.histBins};
+else
+  hist_pars = {a_db};
+end
+
+plots = plot_abstract(testsHists(hist_pars{:}), '', ...
+		      mergeStructs(props, struct('rotateXLabel', 10)));
+
+if isfield(props, 'orient') && strcmp(props.orient, 'x')  
+  extra_props = struct('yLabelsPos', 'left');
+else
+  extra_props = struct;
+end
+
+if ~ isfield(props, 'quiet')
+  all_title = ['Measure histograms for ' get(a_db, 'id') title_str ];
+else
+  all_title = title_str;
+end
+
+if isfield(props, 'orient') && (strcmp(props.orient, 'x')  || ...
+				strcmp(props.orient, 'y'))
+  a_pm = plot_stack(plots, [], props.orient, ...
+		   all_title, mergeStructs(props, extra_props));
+else
+  a_pm = matrixPlots(plots, {}, all_title, props);
+end
 
