853ec1169f8b430f24309e3351922777956ab4db
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-12-08 23:23:43 +0000

    Minor fix and doc update.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@402 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/statsBounds.m
+++ @tests_db/statsBounds.m
@@ -1,13 +1,12 @@
 function a_stats_db = statsBounds(a_db, tests, props)
 
-% statsBounds - Generates a stats_db object with three rows corresponding to 
-%		the mean, min, and max of the tests' distributions. A page is
-%		generated for each page of data in db.
+% statsBounds - Generates a stats_db object with three rows corresponding to the mean, min, and max of the tests' distributions. 
 %
 % Usage:
 % a_stats_db = statsBounds(a_db, tests, props)
 %
 % Description:
+%   A page is generated for each page of data in db.
 %
 %   Parameters:
 %	a_db: A tests_db object.
@@ -26,15 +25,19 @@ if ~ exist('props')
   props = struct([]);
 end
 
+if ~ exist('tests')
+  tests = ':';
+end
+
 cols = tests2cols(a_db, tests);
 
 num_pages = dbsize(a_db, 3);
 pages=1:num_pages;
 data = repmat(0, [3, length(cols), num_pages]);
 for page_num=pages
-  a_page_db = a_db(:, tests, page_num);
-  data(:, :, page_num) = [mean(a_page_db, 1); min(a_page_db.data, [], 1); ...
-			  max(a_page_db.data, [], 1)];
+  a_page_db = onlyRowsTests(a_db, ':', tests, page_num);
+  data(:, :, page_num) = [mean(a_page_db, 1); min(get(a_page_db, 'data'), [], 1); ...
+			  max(get(a_page_db, 'data'), [], 1)];
 end
 
 row_names = {'mean', 'min', 'max'};
