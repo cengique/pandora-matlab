d281ef69dda31fd0b72140b3c7f79f4c7b42f9a8
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-12-08 23:22:17 +0000

    Changed to handle array of DBs to make sure to create same-looking histograms.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@399 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_tests_db/testsHists.m
+++ @params_tests_db/testsHists.m
@@ -21,27 +21,43 @@ function t_hists = testsHists(a_db, num_bins)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/17
 
-tests = fieldnames(get(a_db, 'col_idx'));
-itemIndices = strmatch('ItemIndex', tests);
-
-%# Strip out the RowIndex and ItemIndex columns from criterion db
-tests = setdiff(tests, {'RowIndex', tests{itemIndices}});
-
-%# Preserve original column order (parameters at the beginning)
-cols = sort(tests2cols(a_db, tests));
-
-%# Filter relevant columns
-reduced_db = onlyRowsTests(a_db, ':', cols);
-
-num_params = reduced_db.num_params;
-
-reduced_db = onlyRowsTests(reduced_db, ':', ...
-			   (num_params + 1):(dbsize(reduced_db, 2)));
+num_dbs = length(a_db);
+if num_dbs > 1
+  [reduced_db(1:num_dbs)] = deal(params_tests_db);
+  for db_num=1:num_dbs
+    reduced_db(db_num) = reduce_db(a_db(db_num));
+  end
+  reduced_tests_db = subsref(reduced_db, substruct('.', 'tests_db'));
+else
+  reduced_tests_db = reduced_db.tests_db;
+end
 
 if exist('num_bins')
-  hist_pars = {reduced_db.tests_db, num_bins};
+  hist_pars = {reduced_tests_db, num_bins};
 else
-  hist_pars = {reduced_db.tests_db};
+  hist_pars = {reduced_tests_db};
 end
 
 t_hists = testsHists(hist_pars{:});
+
+end				%# of function
+
+function reduced_db = reduce_db(a_db)
+  tests = fieldnames(get(a_db, 'col_idx'));
+  itemIndices = strmatch('ItemIndex', tests);
+
+  %# Strip out the RowIndex and ItemIndex columns from criterion db
+  tests = setdiff(tests, {'RowIndex', tests{itemIndices}});
+
+  %# Preserve original column order (parameters at the beginning)
+  cols = sort(tests2cols(a_db, tests));
+
+  %# Filter relevant columns
+  reduced_db = onlyRowsTests(a_db, ':', cols);
+  
+  num_params = reduced_db.num_params;
+  
+  reduced_db = onlyRowsTests(reduced_db, ':', ...
+			     (num_params + 1):(dbsize(reduced_db, 2)));
+end
+
