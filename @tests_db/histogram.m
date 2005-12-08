4182e8fe6191ae24da61751fbc68297eb184fc86
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2005-12-08 23:22:54 +0000

    Use same bins when multiple DBs are scanned for histograms.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@400 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/histogram.m
+++ @tests_db/histogram.m
@@ -26,12 +26,40 @@ if ~ exist('num_bins')
   num_bins = 100;
 end
 
+num_dbs = length(db);
+if num_dbs > 1
+  %# If given array of DBs, find maximal bounds to create bins accordingly
+  min_val = +Inf;
+  max_val = -Inf;
+  for db_num=1:num_dbs
+    col_db = onlyRowsTests(db(db_num), ':', col);
+    col_db = onlyRowsTests(col_db, ~ isnan(col_db.data) & ~ isinf(col_db.data), 1);
+    bounds_data = get(statsBounds(col_db), 'data');
+    min_val = min(min_val, bounds_data(2));
+    max_val = max(max_val, bounds_data(3));
+  end
+
+  %# If a scalar, then apply limits and get bins
+  if length(num_bins) == 1
+    num_bins = min_val + (1:num_bins - 1) .* (max_val - min_val) ./ (num_bins - 1);
+  end
+
+  %# It's not A histogram db anymore
+  [a_histogram_db(1:num_dbs)] = deal(histogram_db);
+  for db_num=1:num_dbs
+    %# recurse
+    a_histogram_db(db_num) = histogram(db(db_num), col, num_bins);
+  end  
+
+else
+
 col_db = onlyRowsTests(db, ':', col);
 
 %# Remove NaN or Inf values 
 col_db = onlyRowsTests(col_db, ~ isnan(col_db.data) & ~ isinf(col_db.data), 1);
 %#col_db = col_db( ~ isnan(col_db(:, 1)), 1);
-%# I don't know why the above doesn't work!?
+%# I don't know why the above doesn't work!? 
+%# [ because matlab doesn't call member funcs from here]
 
 %# If any rows left
 if dbsize(col_db, 1) > 0
@@ -46,3 +74,4 @@ col_name = col_name_cell{1};
 
 a_histogram_db = histogram_db(col_name, bins', hist_results', ...
 			      [ col_name ' of ' db.id ]);
+end
