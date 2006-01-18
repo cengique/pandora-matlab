4eb881d16a76ce40371386d24a23659195e14bd1
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-01-18 22:06:16 +0000

    Now returns a crit_bundle. No need to supply both crit_db and crit_bundle anymore.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@430 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @physiol_bundle/matchingRow.m
+++ @physiol_bundle/matchingRow.m
@@ -1,9 +1,9 @@
-function crit_db = matchingRow(a_bundle, traceset_index, props)
+function a_crit_bundle = matchingRow(a_bundle, traceset_index, props)
 
 % matchingRow - Creates a criterion database for matching the neuron at traceset_index.
 %
 % Usage:
-% crit_db = matchingRow(a_bundle, traceset_index, props)
+% a_crit_bundle = matchingRow(a_bundle, traceset_index, props)
 %
 % Description:
 %   Copies selected test values from row as the first row into the 
@@ -15,14 +15,16 @@ function crit_db = matchingRow(a_bundle, traceset_index, props)
 %	props: A structure with any optional properties.
 %		
 %   Returns:
-%	crit_db: A tests_db with two rows for values and STDs.
+%	a_crit_bundle: A dataset_db_bundle with two rows for values and STDs.
 %
 %   Example:
 %	physiol_bundle has an overloaded matchingRow method that
 %	takes the TracesetIndex as argument:
-%	>> crit_db = matchingRow(pbundle, 61)
+%	>> a_crit_bundle = matchingRow(pbundle, 61)
+%	>> a_ranked_bundle = rankMatching(mbundle, a_crit_bundle);
+%	>> printTeXFile(comparisonReport(a_ranked_bundle), 'my_report.tex')
 %
-% See also: rankMatching, tests_db, tests2cols
+% See also: rankMatching, tests_db/matchingRow
 %
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2005/12/21
@@ -34,5 +36,7 @@ end
 j_db = get(a_bundle, 'joined_db');
 
 %# Warning: doesn't call parent function, directly calls tests_db/matchingRow
-crit_db = matchingRow(j_db, find(j_db(:, 'TracesetIndex') == traceset_index), ...
-		      struct('std_db', a_bundle.joined_control_db));
+a_crit_bundle = ...
+    set(a_bundle, 'joined_db', ...
+	matchingRow(j_db, find(j_db(:, 'TracesetIndex') == traceset_index), ...
+		    struct('std_db', a_bundle.joined_control_db)));
