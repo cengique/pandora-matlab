0a0bc77296e0703fa6b147fc3e023365eed67e2b
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-02-07 21:26:51 +0000

    Fixed bunch of bugs. Now works and submits jobs to SGE.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@473 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @script_array_for_cluster/runFirst.m
+++ @script_array_for_cluster/runFirst.m
@@ -24,22 +24,26 @@ function job_results = runFirst(a_script_cluster)
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2006/02/01
 
+eol_str = sprintf('\n');
+
 %# Save this object into a file
-save [get(a_script_cluster, 'id') '.mat' ] a_script_cluster
+save([get(a_script_cluster, 'id') '.mat' ], 'a_script_cluster');
 
 %# Submit a SGE job for the array jobs
 load_obj = ['load ' get(a_script_cluster, 'id') '.mat; '];
 array_job_command = ...
     ['qsub -t 1:' num2str(get(a_script_cluster, 'num_runs')) ' ' ...
-     a_script_cluster.sge_wrapper_script ' ''' load_obj ...
-     'runJob(a_script_cluster, %d)''' ];
+     a_script_cluster.sge_wrapper_script ' "' load_obj ...
+     'runJob(a_script_cluster, %d)"' ];
 [s, w] = system(array_job_command);
 
 if s == 0
   %# Success, look for SGE job number
   [array_job_number] = sscanf(w, 'Your job %d');
 
-  %# Check if mail notofocation is requested
+  disp(['Submitted job ' num2str(array_job_number) ': ' eol_str array_job_command]);
+
+  %# Check if mail notification is requested
   a_props = get(a_script_cluster, 'props');
   if isfield(a_props, 'notifyByMail')
     sge_opts = ['-m ea -M ' a_props.notifyByMail];
@@ -50,16 +54,20 @@ if s == 0
   %# Then submit the last job as dependent on the array job
   last_job_command = ...
       ['qsub -hold_jid ' num2str(array_job_number) ' ' sge_opts ' ' ...
-       a_script_cluster.sge_wrapper_script ' ''' load_obj ...
-       'runLast(a_script_cluster, [])''' ];
+       a_script_cluster.sge_wrapper_script ' "' load_obj ...
+       'runLast(a_script_cluster, [])"' ];
   [s, w] = system(last_job_command);
   if s == 0
     %# Success, look for SGE job number
     [last_job_number] = sscanf(w, 'Your job %d');
+    disp(['Submitted last job ' num2str(last_job_number) ': ' eol_str last_job_command]);
+
   else
-    error(['Could not submit last job!' sprintf('\n') last_job_command ]);
+    error(['Could not submit last job!' eol_str last_job_command eol_str ...
+	   'Error: ' w ]);
   end
 
 else
-  error([ 'Could not submit array job!' sprintf('\n') array_job_command ]);
+  error([ 'Could not submit array job!' sprintf('\n') array_job_command eol_str ...
+	 'Error: ' w ]);
 end
