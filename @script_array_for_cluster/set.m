ac875389388f2c3944f01251ee2725565542b9d2
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-02-07 21:25:10 +0000

    Getting and setting member variables works.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@472 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @script_array_for_cluster/set.m
@@ -0,0 +1,9 @@
+function a = set(a, attr, val)
+% set - Generic method for setting object attributes.
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2006/02/06
+try
+  a.(attr) = val;
+catch
+  %# delegate to upper level
+  a.script_array = set(a.script_array, attr, val);
+end
