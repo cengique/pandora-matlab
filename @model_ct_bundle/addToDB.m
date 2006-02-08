572e05d1e02d0358d006e91b1c9c7a3c1635211a
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-02-08 23:13:15 +0000

    Initial version, not tested.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@477 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @model_ct_bundle/addToDB.m
@@ -0,0 +1,48 @@
+function a_mbundle = addToDB(a_mbundle, a_raw_db, props)
+
+% addToDB - Caoncatenate to existing DB in the bundle.
+%
+% Usage:
+% a_mbundle = addToDB(a_mbundle, a_raw_db, props)
+%
+% Description:
+%   If joinedDb is not given, calls treatSimDB to get the joined_db from this raw DB. 
+% Then concats to both db and joined_db in bundle.
+%
+% Parameters:
+%	a_mbundle: A model_ct_bundle object.
+%	a_crit_bundle: A physiol_bundle having a crit_db as its joined_db.
+%	props: A structure with any optional properties.
+%	  joinedDb: The joined version of a_raw_db.
+%	  dataset: If given, this one is used to replace the fileset in the bundle.
+%		
+% Returns:
+%	a_mbundle: a model_ct_bundle object containing the added DB.
+%
+% Example: 
+% >> mbundle = addToDB(mbundle, params_tests_db(mfileset, [19684:59956]))
+%
+% See also: params_tests_fileset/addFiles, multi_fileset_gpsim_cns2005/addFileDir
+%
+% $Id$
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2006/02/06
+
+if ~ exist('props')
+  props = struct([]);
+end
+
+if isfield(props, 'joinedDb')
+  j_db = props.joinedDb;
+else
+  j_db = treatSimDB(a_raw_db);
+end
+
+if isfield(props, 'dataset')
+  a_mbundle = set(a_mbundle, 'dataset', props.dataset);
+end
+
+a_mbundle = set(a_mbundle, 'db' , [get(a_mbundle, 'db'); ...
+				   a_raw_db(:, {'pAcip', 'trial', 'ItemIndex'})]);
+a_mbundle = set(a_mbundle, 'joined_db', ...
+		[ get(a_mbundle, 'db'); j_db ]) ;
+
