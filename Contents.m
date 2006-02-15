b9e794bb9d045550dd337c2176df7b7b909a724a
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-02-15 22:33:05 +0000

    Updated documentation and labeled for version 0.9b.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@490 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- Contents.m
+++ Contents.m
@@ -4,23 +4,23 @@
 % subclassed to take specific tasks. They should not be modified for
 % more specialized tasks.
 %
-% Basic data wrapper classes that define tests:
+% Basic data wrapper classes that define the measurements:
 %   trace		- A voltage/current trace.
 %   spikes		- Spike times of a trace. 
 %   spike_shape		- Averaged spike shape from a trace.
-%   period		- Define time periods.
+%   period		- Defines time periods to operate on trace or spikes objects.
 %
 % Classes specialized for CIP experiments tests:
 %   cip_trace		- A voltage trace with a CIP applied.
-%   cip_traces		- Traces with a set of CIPs applied (from /Lab/matlab/cengiz).
 %
 % Profile classes that hold test results:
 %   result_profile	- Base class that holds a results structure.
 %   trace_profile	- Generic example class for holding a trace profile.
 %   cip_trace_profile	- Holds cip_trace results. Template class designed 
 %			only for subclassing.
-%   cip_traces_profile	- Vector/polynomial results profile from a 
-%			cip_traces.
+%   cip_trace_allspikes_profile 
+%			- Created by cip_trace/getProfileAllSpikes, contains spike shape
+%			  from individual spikes.
 %   params_tests_profile- Collects both intermediate and result data structures 
 %			from analysis of a params_tests_db.
 %
@@ -29,19 +29,45 @@
 %   params_tests_fileset - Holds a list of filenames and associated information,  
 %			   capable of creating a params_tests_db.
 %   params_cip_trace_fileset - Fileset from which cip_trace objects can be created. 
-%   cip_traces_dataset  - Dataset of cip_traces objects.
 %   physiol_cip_traceset - Dataset of a traceset from a single file.
 %   physiol_cip_traceset_fileset - Dataset of a tracesets from many files.
 % 
-% Profile database classes created from datasets:
-%   tests_db		- General test results database class that 
-%			contains many utilities.
+% Database classes created from datasets:
+%   tests_db		- Base class for databases. Contains many utilities.
 %   params_tests_db	- DB extended to hold parameter values 
 %			associated with results.
+%   spikes_db		- Holds measures from each individual spike in a trace.
 %   test_3D_db		- 3D database of tests that vary with a third variable.
+%   corrcoefs_db	- Holds correlation coefficients.
+%   histogram_db	- Holds histogram bins.
+%   stats_db		- Holds statistical measurements.
+%   ranked_db		- Database ranked for a criterion, holds error values or distances.
+%
+% Bundle classes that bridge the gap between database and it's ancestral dataset:
+%   dataset_db_bundle	- Base class of bundles. Puts a processed and raw DBs with datasets.
+%   model_ct_bundle	- Bundle for model databases.
+%   physiol_bundle	- Bundle for physiology databases.
+%
+% Plotting classes:
+%   plot_abstract	- Base class that holds information sufficient to generate any plot.
+%   plot_simple		- Simple extension that works for most simple plots.
+%   plot_stack		- Holds horizontal or vertical stack of plot_abstract objects.
+%   plot_superpose	- Allows superposing different plot_abstract's in the same axis.
+%   plot_bars		- Bar plot with extended errorbars.
+%   plot_errorbar	- Errorbar plot.
+%   plot_errorbars	- Multi-axis errorbar plot.
+%
+% Classes for generating formatted printable or presentable documents:
+%   doc_generate	- Base class for all document classes.
+%   doc_plot		- Holds a plot_abstract and captions, etc.
+%   doc_multi		- Combines multiple doc_generate objects.
+%
+% Script control classes for cluster computing:
+%   script_factory	- Generates a set of scripts based on a recipe.
+%   script_array	- Designed to execute an array job serially on a computer.
+%   script_array_for_cluster - Executes an array job on a cluster.
 %
 % Notes:
-% N/I: Not implemented (yet).
 %
 % See also: subclasses under classes/cengiz
 %
