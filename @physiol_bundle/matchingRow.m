85407730c0a35d37cf184efedf65b7c8f3b50a00
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-02-15 21:07:08 +0000

    Now returns a crit_db rather than a crit_bundle. Unnecessary bundling needs to be avoided for memory reasons.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@480 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @physiol_bundle/matchingRow.m
+++ @physiol_bundle/matchingRow.m
@@ -1,21 +1,21 @@
-function a_crit_bundle = matchingRow(a_bundle, traceset_index, props)
+function a_crit_db = matchingRow(p_bundle, traceset_index, props)
 
 % matchingRow - Creates a criterion database for matching the neuron at traceset_index.
 %
 % Usage:
-% a_crit_bundle = matchingRow(a_bundle, traceset_index, props)
+% a_crit_db = matchingRow(p_bundle, traceset_index, props)
 %
 % Description:
 %   Copies selected test values from row as the first row into the 
 % criterion db. Adds a second row for the STD of each column in the db.
 %
 %   Parameters:
-%	a_bundle: A physiol_bundle object.
+%	p_bundle: A physiol_bundle object.
 %	traceset_index: A TracesetIndex of the neuron and treatments to match.
 %	props: A structure with any optional properties.
 %		
 %   Returns:
-%	a_crit_bundle: A dataset_db_bundle with two rows for values and STDs.
+%	a_crit_db: A tests_db with two rows for values and STDs.
 %
 %   Example:
 %	physiol_bundle has an overloaded matchingRow method that
@@ -38,11 +38,11 @@ if num_tracesets > 1
   %# If called with vectorized indices
   [ a_crit_bundle(1:num_tracesets) ] = deal(physiol_bundle);
   for traceset_num = 1:num_tracesets
-    a_crit_bundle(traceset_num) = matchingRow(a_bundle, traceset_index(traceset_num), props);
+    a_crit_bundle(traceset_num) = matchingRow(p_bundle, traceset_index(traceset_num), props);
   end
 else
   %# Called with one index
-  j_db = get(a_bundle, 'joined_db');
+  j_db = get(p_bundle, 'joined_db');
 
   row_num = find(j_db(:, 'TracesetIndex') == traceset_index);
 
@@ -50,15 +50,10 @@ else
     error(['Cannot find TracesetIndex ' num2str(traceset_index) ]);
   end
 
-  %# Get crit_db and rename to a more human-readable form
-  crit_db = set(matchingRow(j_db, row_num, ...
-			    struct('std_db', a_bundle.joined_control_db)), 'id', ...
-		[ 'Matching traceset ' ...
-		 num2str(traceset_index) ' of neuron ' ...
-		 get(getItem(get(a_bundle, 'dataset'), traceset_index), 'id') ...
-		 ' of ' j_db.id ]);
-  clear j_db
-  
-  %# Warning: doesn't call parent function, directly calls tests_db/matchingRow
-  a_crit_bundle = set(a_bundle, 'joined_db', crit_db );
+  %# Get a_crit_db and rename to a more human-readable form
+  a_crit_db = set(matchingRow(j_db, row_num, ...
+			      struct('std_db', p_bundle.joined_control_db)), 'id', ...
+		  ['Matching traceset ' num2str(traceset_index) ' of neuron ' ...
+		   get(getItem(get(p_bundle, 'dataset'), traceset_index), 'id') ...
+		   ' of ' j_db.id ]);
 end
