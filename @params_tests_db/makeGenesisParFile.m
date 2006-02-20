1c49b6cb84288d6f84515b50fda91236e6869b4d
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-02-20 21:52:24 +0000

    Added optional property to add a counting trial parameter to the file.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@493 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_tests_db/makeGenesisParFile.m
+++ @params_tests_db/makeGenesisParFile.m
@@ -1,9 +1,9 @@
-function makeGenesisParFile(a_db, filename)
+function makeGenesisParFile(a_db, filename, props)
 
 % makeGenesisParFile - Creates a Genesis parameter file with all the parameter values in a_db.
 %
 % Usage:
-% makeGenesisParFile(a_db, filename)
+% makeGenesisParFile(a_db, filename, props)
 %
 % Description:
 %   For each a_db row, print the parameter names in a
@@ -12,20 +12,43 @@ function makeGenesisParFile(a_db, filename)
 %   Parameters:
 %	a_db: A params_tests_db object.
 %	filename: Genesis parameter file to be created.
+%	props: A structure with any optional properties.
+%	  trialStart: If given, adds/replaces the trial parameter and counts forward.
 %
 %   Returns:
 %	nothing.
 %
 %   Example:
-%	>> blocked_rows_db = getBlockedParamRows(ranked_for_gps0501a_db, 1, [1, 2], 10, [-100 100]);
-%	>> makeGenesisParFile(blocked_rows_db, 'blocked_gps0501-03.par')
+%>> blocked_rows_db = makeModifiedParamDB(ranked_for_gps0501a_db, 1, [1, 2], 10, [-100 100]);
+%>> makeGenesisParFile(blocked_rows_db, 'blocked_gps0501-03.par')
 %
-% See also: params_tests_db/getBlockedParamRows
+% See also: makeModifiedParamDB, scanParamAllRows, scaleParamsOneRow
 %
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2005/03/13
 
 %# TODO: read paramRanges.txt to verify parameter sequence
+
+if ~exist('props')
+  props = struct;
+end
+
+%# Add/replace a trial column and count forward
+if isfield(props, 'trialStart')
+  start_trial = props.trialStart;
+  trial_col = (start_trial:(start_trial - 1 + dbsize(a_db, 1)))';
+  try 
+    a_db = addParams(a_db, {'trial'}, trial_col);
+  catch
+    lerr = lasterror;
+    %# If column already exists, it's ok
+    if strcmp(lerr.identifier, 'tests_db:col_exists')
+      a_db = assignRowsTests(a_db, trial_col, ':', 'trial');
+    else
+      rethrow(lerr);      
+    end
+  end
+end
 
 num_rows = dbsize(a_db, 1);
 
