7803423e5e349bb8e992b70280dc2b6f6c56366b
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-03-13 22:57:36 +0000

    Works.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@512 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @plot_abstract/subsasgn.m
@@ -0,0 +1,27 @@
+function a = subsasgn(a, index, val)
+% subsasgn - Defines generic index-based assignment for objects.
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2006/02/06
+
+%# If a is an array, use built-in methods
+num = length(a);
+if num == 0
+  a = builtin('subsasgn', val, index, val);
+  return;
+elseif length(a) > 1
+  a = builtin('subsasgn', a, index, val);
+  return;
+end
+
+if size(index, 2) > 1
+  %# recursive if multiple indices
+  a = subsasgn(a, index(1), subsasgn(subsref(a, index(1)), index(2:end), val));
+else
+  switch index.type
+    case '()'
+      error(['Operator () not defined for ' class(a) '.']);
+    case '.'
+      a = set(a, index.subs, val);
+    case '{}'
+      error(['Operator {} not defined for ' class(a) '.']);
+  end
+end
