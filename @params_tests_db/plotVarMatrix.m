ff2d9daf41bb74da3cba586eed18e8c2f347b70b
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-05-12 20:35:34 +0000

    Added optional parameters, but no specific option. Got title from stats_sb directly.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@531 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_tests_db/plotVarMatrix.m
+++ @params_tests_db/plotVarMatrix.m
@@ -1,25 +1,30 @@
-function a_plot_stack = plotVarMatrix(a_db, p_stats)
+function a_plot_stack = plotVarMatrix(a_db, p_stats, props)
 
-% plotVarMatrix - Create a stack of parameter-test variation plots 
-%		organized in a matrix.
+% plotVarMatrix - Create a stack of parameter-test variation plots organized in a matrix.
 %
 % Usage:
-% a_plot_stack = plotVarMatrix(a_db, p_stats)
+% a_plot_stack = plotVarMatrix(a_db, p_stats, props)
 %
 % Description:
 %   Skips the 'ItemIndex' test.
 %
 %   Parameters:
 %	a_db: A params_tests_db object.
-%	p_stats: Cell array of invariant parameter databases.
+%	p_stats: Cell array of invariant parameter databases obtained from
+%		calling paramsTestsHistsStats.
+%	props: A structure with any optional properties, passed to plot_stack.
 %		
 %   Returns:
 %	a_plot_stack: A plot_stack with the plots organized in matrix form
 %
-% See also: params_tests_profile, plotVar
+% See also: paramsTestsHistsStats, params_tests_profile, plotVar.
 %
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/17
+
+if ~ exist('props')
+  props = struct([]);
+end
 
 num_params = a_db.num_params;
 num_tests = dbsize(a_db, 2) - num_params; %# Except the item indices
@@ -58,8 +63,11 @@ for test_num=1:num_tests
   plot_rows{test_num} = plot_stack(plots, [NaN NaN ranges(3:4)], 'x', '', props);
 end
 
+title_str = [ p_stats{1}.id ];
+%# 'Measure Variations with Parameter Values'
+
 a_plot_stack = plot_stack(plot_rows, [], 'y', ...
-			  'Measure Mean and SE Variations with Parameter Values', ...
-			  struct('titlesPos', 'none', ...
-				 'xLabelsPos', 'bottom', ...
-				 'xTicksPos', 'bottom'));
+			  title_str, ...
+			  mergeStructs(props, struct('titlesPos', 'none', ...
+						     'xLabelsPos', 'bottom', ...
+						     'xTicksPos', 'bottom')));
