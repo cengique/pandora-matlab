55a92ec9685ef8b2c7c43dc1086e961b1324d819
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-06-09 19:44:27 +0000

    Added support for vector inputs.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@599 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @stats_db/subsref.m
+++ @stats_db/subsref.m
@@ -1,5 +1,14 @@
 function b = subsref(a,index)
 % subsref - Defines generic indexing for objects.
+
+%# What's the purpose of this method here, since tests_db/subsref exists?
+
+%# If a is an array, use built-in methods
+if length(a) > 1
+  b = builtin('subsref', a, index);
+  return;
+end
+
 if size(index, 2) > 1
   first = subsref(a, index(1));
   %# recursive
@@ -8,7 +17,7 @@ else
   switch index.type
     case '()'
       %# Delegate to base class
-      b = a.tests_3D_db(index.subs{:});
+      b = onlyRowsTests(a.tests_3D_db, index.subs{:});
     case '.'
       b = get(a, index.subs); 
       %# eval(['a.' index.subs]);
