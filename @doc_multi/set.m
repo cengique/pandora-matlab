451b7da0995430c14b8fc8f7d04cf8ec36860ea6
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-06-09 19:18:03 +0000

    Generic methods.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@581 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ @doc_multi/set.m
@@ -0,0 +1,8 @@
+function a = set(a, attr, val)
+% set - Generic method for setting object attributes.
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
+try
+  a.(attr) = val;
+catch
+  a.doc_generate = set(a.doc_generate, attr, val);
+end
