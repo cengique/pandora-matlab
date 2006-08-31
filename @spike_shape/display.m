aad6566906848566a018b28a1860f756156e8011
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-08-31 23:54:55 +0000

    improved display method.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@649 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @spike_shape/display.m
+++ @spike_shape/display.m
@@ -3,7 +3,14 @@ function s = display(t)
 % Generic object display method.
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/08/04
 
-disp(sprintf('%s, %s', class(t), get(t, 'id')));
-
-display(t.trace);
+num = size(t);
+if max(num) > 1
+  s = [ '[' sprintf('%dx', num(1:(end-1))) num2str(num(end)) ' ' class(t) ']' ];
+  disp(s);
+  struct(t(1))
+else
+  s = sprintf('%s', t.trace.id);
+  disp(s);
+  struct(t)
+end
 
