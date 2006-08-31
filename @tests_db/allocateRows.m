37c83a0330891ba30add08ef63d760af47ae3f6c
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-08-31 23:57:41 +0000

    fixed doc, logic and bugs.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@654 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/allocateRows.m
+++ @tests_db/allocateRows.m
@@ -3,14 +3,13 @@ function obj = allocateRows(obj, num_rows)
 % allocateRows - Preallocates a NaN-filled num_rows rows in tests_db.
 %
 % Usage:
-% index = allocateRows(obj, num_rows)
+% obj = allocateRows(obj, num_rows)
 %
 % Description:
-%   Allocates the desired number of rows to speed up filling up the data 
-%   matrix. Using addRow after this operation is still expensive.
-%   The method of allocating a matrix, filling it up, and
-%   then providing it to the tests_db constructor is the preferred method 
-%   of creating tests_db objects.
+%   Allocates the desired number of rows to speed up filling up the data matrix 
+%   using assignRowsTests. Using addRow after this operation is still expensive.
+%   The method of allocating a matrix, filling it up, and then providing it to 
+%   the tests_db constructor is the preferred method of creating tests_db objects.
 %
 %   Parameters:
 %	obj: A tests_db object.
@@ -19,14 +18,14 @@ function obj = allocateRows(obj, num_rows)
 %   Returns:
 %	obj: The new tests_db object.
 %
-% See also: addRow, setRows, tests_db
+% See also: assignRowsTests, addRow, setRows, tests_db
 %
 % $Id$
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/08
 
-if dbsize(obj, 1) ~= 0 || dbsize(obj, 2) ~= 0
+if dbsize(obj, 1) ~= 0 && dbsize(obj, 2) ~= 0
   error(['Database is not empty! This operation erases ', ...
 	 'all contents of the DB.']);
 end
 
-obj.data = repmat(NaN, rows, length(fieldnames(obj.col_idx)));
+obj.data = repmat(NaN, num_rows, length(fieldnames(obj.col_idx)));
