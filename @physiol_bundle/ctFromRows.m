a775ab0b30d7d72d762892a7eaab268c5a5c0def
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2006-08-31 22:25:02 +0000

    added control of traes to include.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@633 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @physiol_bundle/ctFromRows.m
+++ @physiol_bundle/ctFromRows.m
@@ -13,6 +13,7 @@ function a_cip_trace = ctFromRows(a_pbundle, a_db, cip_levels, props)
 %	traceset_idx: A column vector with traceset indices.
 %	cip_levels: A column vector of CIP-levels to be loaded.
 %	props: A structure with any optional properties.
+%	  traces: column vector of trace indices to load.
 %	  showParamsList: Cell array of params or treatments to include in the id field.
 %		
 %   Returns:
@@ -28,13 +29,17 @@ if ~exist('props')
 end
 
 if isa(a_db, 'tests_db')
-  traceset_vals = onlyRowsTests(a_db, 1, 'TracesetIndex');
+  traceset_vals = get(onlyRowsTests(a_db, ':', 'TracesetIndex'), 'data');
 else
   traceset_vals = a_db;
 end
 
 p_db = get(a_pbundle, 'db');
 rows = onlyRowsTests(p_db, ':', 'TracesetIndex') == traceset_vals;
+
+if isfield(props, 'traces') 
+  rows = rows & onlyRowsTests(p_db, ':', 'ItemIndex') == props.traces;
+end
 
 if ~ isempty(cip_levels) 
   rows = rows & onlyRowsTests(p_db, ':', 'pAcip') == cip_levels;
