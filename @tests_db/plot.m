b7dcf76e99dd817236d3b0332bbb6b71efd75890
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-01-24 17:44:50 +0000

    passes props
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@709 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/plot.m
+++ @tests_db/plot.m
@@ -1,16 +1,17 @@
-function h = plot(a_tests_db, title_str)
+function h = plot(a_tests_db, title_str, props)
 
 % plot - Generic method to plot a tests_db or a subclass. Requires a 
 %	plot_abstract method to be defined for this object.
 %
 % Usage:
-% h = plot(a_tests_db, title_str)
+% h = plot(a_tests_db, title_str, props)
 %
 % Description:
 %
 %   Parameters:
 %	a_tests_db: A histogram_db object.
 %	title_str: (Optional) String to append to plot title.
+%	props: A structure with any optional properties, passed to plot_abstract.
 %		
 %   Returns:
 %	h: The figure handle created.
@@ -25,4 +26,8 @@ if ~ exist('title_str')
   title_str = '';
 end
 
-h = plotFigure(plot_abstract(a_tests_db, title_str));
+if ~ exist('props')
+  props = struct;
+end
+
+h = plotFigure(plot_abstract(a_tests_db, title_str, props));
