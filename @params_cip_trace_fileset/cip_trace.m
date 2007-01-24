c5be72035554ea1368798c36197e7dd2543aa68e
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-01-24 17:38:58 +0000

    added neuronLabel prop for specifying a display name for ct.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@702 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @params_cip_trace_fileset/cip_trace.m
+++ @params_cip_trace_fileset/cip_trace.m
@@ -1,9 +1,9 @@
-function a_cip_trace = cip_trace(fileset, file_index)
+function a_cip_trace = cip_trace(fileset, file_index, props)
 
 % cip_trace - Loads raw cip_traces for each given file_index in this fileset.
 %
 % Usage:
-% a_cip_trace = cip_trace(fileset, file_index|a_db)
+% a_cip_trace = cip_trace(fileset, file_index|a_db, props)
 %
 % Description:
 %
@@ -11,6 +11,8 @@ function a_cip_trace = cip_trace(fileset, file_index)
 %	fileset: A params_tests_fileset.
 %	file_index: A single or array of indices of files in fileset.
 %	a_db: A DB created by this fileset to read the item indices from.
+%	props: A structure with any optional properties.
+%	  neuronLabel: Used for annotation purposes.
 %		
 %   Returns:
 %	a_cip_trace: A cip_trace object.
@@ -21,11 +23,15 @@ function a_cip_trace = cip_trace(fileset, file_index)
 %
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/13
 
+if ~exist('props')
+  props = struct;
+end
+
 if isa(file_index, 'tests_db')
   a_db = file_index;
   col_data = get(onlyRowsTests(a_db, ':', 'ItemIndex'), 'data');
   %# recurse
-  a_cip_trace = cip_trace(fileset, col_data);
+  a_cip_trace = cip_trace(fileset, col_data, props);
 else
   num_indices = length(file_index);
   a_cip_trace(1:num_indices) = cip_trace;
@@ -33,24 +39,24 @@ else
     filename = getItem(fileset, file_index(an_index));
     fullname = fullfile(get(fileset, 'path'), filename);
 
-    props = get(fileset, 'props');
-    if isfield(props, 'param_rows')
+    f_props = get(fileset, 'props');
+    if isfield(f_props, 'param_rows')
       %# Take parameter values from the specified parameter file,
       %# in addition to the ones specified on data filenames.
       names_vals = parseGenesisFilename(fullname);
-      if isfield(props, 'num_params')
-	num_params = props.num_params;
+      if isfield(f_props, 'num_params')
+	num_params = f_props.num_params;
       else
 	num_params = size(names_vals, 1);
       end
-      if ~ isfield(props, 'param_trial_name')
-	props.param_trial_name = 'trial';
+      if ~ isfield(f_props, 'param_trial_name')
+	f_props.param_trial_name = 'trial';
       end
-      str_index = strmatch(props.param_trial_name, names_vals{1:num_params, 1});
+      str_index = strmatch(f_props.param_trial_name, names_vals{1:num_params, 1});
       
       if length(str_index) < 1
 	error(['Parameter lookup from rows is requested, but cannot find ' ...
-	       'the "' props.param_trial_name '" parameter in the data filename ' fullname ]);
+	       'the "' f_props.param_trial_name '" parameter in the data filename ' fullname ]);
       end
     
       trial_num = names_vals{str_index, 2};
@@ -58,12 +64,17 @@ else
     else
       trace_id = num2str(file_index(an_index));
     end
+
+    %# overwrite the above trace id if one specified in neuronLabel
+    if isfield(props, 'neuronLabel')
+      trace_id = props.neuronLabel;
+    end
     
     %# Load a cip_trace object
     a_cip_trace(an_index) = ...
 	cip_trace(fullname, get(fileset, 'dt'), get(fileset, 'dy'), ...
 		  fileset.pulse_time_start, fileset.pulse_time_width, ...
 		  [get(fileset, 'id') '(' trace_id ')'], ...
-		  get(fileset, 'props'));
+		  mergeStructs(props, get(fileset, 'props')));
   end
 end
