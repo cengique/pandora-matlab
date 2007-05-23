8261c9a220f13faf374d0a9dafc1309b50d38ccd
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-05-23 19:10:05 +0000

    added optional label
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@738 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/plotXRows.m
+++ @tests_db/plotXRows.m
@@ -12,7 +12,9 @@ function a_p = plotXRows(a_db, test_y, title_str, short_title, props)
 %	test_y: Y variable.
 %	title_str: (Optional) A string to be concatanated to the title.
 %	short_title: (Optional) Few words that may appear in legends of multiplot.
-%	props: A structure with any optional properties passed to plotScatter.
+%	props: A structure with any optional properties passed to
+%		plotScatter.
+%	  RowName: Label to show on X-axis (default='RowNumber')
 %		
 %   Returns:
 %	a_p: A plot_abstract.
@@ -36,7 +38,11 @@ if ~ exist('props')
 end
 
 %# Add a new column that keeps count of rows and call plotScatter
-new_col_name = 'RowNumber';
+if isfield(props, 'RowName')
+    new_col_name = props.RowName;
+else
+    new_col_name = 'RowNumber';
+end
 col_added_db = addColumns(a_db, {new_col_name}, (1:dbsize(a_db, 1))');
 
 a_p = plotScatter(col_added_db, new_col_name, test_y, title_str, short_title, props);
