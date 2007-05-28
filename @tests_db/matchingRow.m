3fc15704d936521745abf9372358f957a8de3650
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-05-28 18:19:38 +0000

    Renamed std_db to distDB. Calculates covariance matrix and keeps it in props.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@745 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/matchingRow.m
+++ @tests_db/matchingRow.m
@@ -6,14 +6,15 @@ function crit_db = matchingRow(db, row, props)
 % crit_db = matchingRow(db, row, props)
 %
 % Description:
-%   Copies selected test values from row as the first row into the 
-% criterion db. Adds a second row for the STD of each column in the db.
+%   Copies selected test values from row as the first row into the criterion
+% db. Adds a second row for the STD of each column in the db.  Calculates the
+% covariance for using the Mahalonobis distance in the ranking.
 %
 %   Parameters:
 %	db: A tests_db object.
 %	row: A row index to match.
 %	props: A structure with any optional properties.
-%		std_db: Take the standard deviation from this db instead.
+%	  distDB: Take the standard deviation and covariance of this db instead.
 %		
 %   Returns:
 %	crit_db: A tests_db with two rows for values and STDs.
@@ -34,23 +35,30 @@ end
 
 tests = ':';
 
+% Get the selected row in a DB
 cols = tests2cols(db, tests);
-col_db = onlyRowsTests(db, row, tests);
+crit_db = onlyRowsTests(db, row, tests);
 
-%# Check for std_db
-if isfield(props, 'std_db')
-  std_db = props.std_db;
+%# Check for distDB
+if isfield(props, 'distDB')
+  dist_db = props.distDB;
 else
-  std_db = db;
+  dist_db = db;
 end
 
-%# Add the row index
-col_added_db = addColumns(col_db, {'RowIndex'}, row);
+% Calculate covariance for using Mahalonobis distance
+cov_db = cov(dist_db(:, tests));
 
-%# Add the STD row
-std_db = addLastRow(col_added_db, [ get(std(std_db(:, tests)), 'data'), 0 ]);
+% add the covariance matrix into the props
+crit_db = setProp(crit_db, 'cov', cov_db);
+
+%# Add the row index
+crit_db = addColumns(crit_db, {'RowIndex'}, row);
+
+%# Add the STD row, calculating from cov_db
+crit_db = addLastRow(crit_db, [ sqrt(diag(get(cov_db, 'data')))', 0 ]);
 
 %# Adjust other fields
-crit_db = set(std_db, 'row_idx', makeIdx({'Values', 'STD'}));
+crit_db = set(crit_db, 'row_idx', makeIdx({'Values', 'STD'}));
 crit_db = set(crit_db, 'id', ['Criterion for matching row ' num2str(row) ...
 			      ' from ' db.id ]);
