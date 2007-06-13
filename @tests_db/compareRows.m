7d573f41e2aaab203f1a484b5aed9871294c73b7
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-06-13 16:59:59 +0000

    Allow comparing columns, not just rows. Also optimized comparison algorithm.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@781 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @tests_db/compareRows.m
+++ @tests_db/compareRows.m
@@ -1,22 +1,23 @@
-function [rows, compared] = compareRows(db, row)
+function [idx, compared] = compareRows(db, row)
 
 % compareRows - Returns comparison results of db and the given row in a column vector.
 %
 % Usage:
-% rows = compareRows(db, row)
+% idx = compareRows(db, row)
 %
 % Description:
 % If the row argument has multiple rows, the comparison is done separately
 % for each of its rows and the results are the logical AND of those.
 % Note that, it uses summation of distance for magnitude comparison. 
-% That is, all columns have the same weight.
+% That is, all columns have the same weight. If the db contains many
+% columns and one row, then the columns are compared instead of rows.
 %
 %   Parameters:
 %	db: A tests_db object.
 %	row: Row array, matrix or database to be compared with db rows.
 %		
 %   Returns:
-%	rows: A inverted logical column vector of comparison results. 
+%	idx: A inverted logical column vector of comparison results. 
 %		(false if db == row, true otherwise)
 %
 % See also: eq, tests_db
@@ -30,6 +31,11 @@ if isa(row, 'tests_db')
   row = row.data;
 end
 
+% flip to do columns instead of rows
+if dbsize(db, 2) > dbsize(db, 1) && dbsize(db, 1) == 1
+  db = transpose(db);
+end
+
 if dbsize(db, 2) ~= size(row, 2)
   error('Row must contain same columns as the db.');
 end
@@ -38,22 +44,19 @@ end
 num_rows = size(row, 1);
 num_db_rows = dbsize(db, 1);
 ones_matx = ones(num_db_rows, 1);
-rows = true(num_db_rows, 1);
+idx = true(num_db_rows, 1);
 
 %# Calculate multiple rows by tail recursion function 
+% TODO: remove function call to speed it up
 for row_num=1:num_rows
-  rows = rows & doOneRow(row(row_num, :));
-end
-
-function from_onerow = doOneRow(onerow)
   %# Find doesn't work in two dimension comparisons
   %# Thus, use algorithm:
 
   %# - duplicate row to a matrix of same size with db
   %# - subtract from db
-  compared = db.data - (ones_matx * onerow);
+  compared = db.data - (ones_matx * row(row_num, :));
 
-  from_onerow = sum(abs(compared), 2) > eps(0);
+  idx = idx & (sum(abs(compared), 2) > eps(0));
 end
 
 end
