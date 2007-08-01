b193c447fc2ce6f6ab903c04c8aa55e4c043fec7
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-08-01 19:31:55 +0000

    bugfix for missing special measures case.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/trunk@794 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- @ranked_db/renameColumns.m
+++ @ranked_db/renameColumns.m
@@ -6,8 +6,9 @@ function a_db = renameColumns(a_db, test_names, new_names)
 % a_db = renameColumns(a_db, test_names, new_names)
 %
 % Description:
-%   This method is an overloaded method for ranked_db that keeps the column names
-% of the ranked, criterion and original DBs consistent.
+%   This method is an overloaded method for ranked_db that keeps consistent
+% the column names of the ranked, criterion and original DBs. The other
+% DBs are not renamed for the Distance and RowIndex columns.
 %
 % Parameters:
 %	a_db: A ranked_db object.
@@ -26,5 +27,16 @@ function a_db = renameColumns(a_db, test_names, new_names)
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2006/06/07
 
 a_db.tests_db = renameColumns(a_db.tests_db, test_names, new_names);
-a_db.orig_db = renameColumns(a_db.orig_db, test_names, new_names);
-a_db.crit_db = renameColumns(a_db.crit_db, test_names, new_names);
+
+%if ~ iscell(test_names) || length(test_names) == 1
+%  test_names = { test_names };
+%end
+
+[some_test_names, idx] = setdiff(test_names, {'Distance', 'RowIndex'});
+
+if ~isempty(idx)
+  a_db.orig_db = ...
+      renameColumns(a_db.orig_db, test_names(idx), new_names(idx));
+  a_db.crit_db = ...
+      renameColumns(a_db.crit_db, test_names(idx), new_names(idx));
+end
