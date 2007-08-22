51ca337753f0975cc9c0620deec6015d1dc468bf
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-08-22 19:47:24 +0000

    doc update/fix
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@817 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@stats_db/compareStats.m
+++ classes/@stats_db/compareStats.m
@@ -3,17 +3,16 @@ function a_mult_stats_db = compareStats(a_stats_db, varargin)
 % compareStats - Merges multiple stats_dbs into pages of a single stats_db for comparison.
 %
 % Usage:
-% a_mult_stats_db = compareStats(a_stats_db)
+% a_mult_stats_db = compareStats(a_stats_db, a_2nd_stats_db, ...)
 %
 % Description:
 %   Generates a plot_simple object from this histogram.
 %
 %   Parameters:
-%	a_stats_db: A histogram_db object.
-%	command: Plot command (Optional, default='bar')
+%	a_stats_db: A stats_db object.
 %		
 %   Returns:
-%	a_mult_stats_db: A object of compareStats or one of its subclasses.
+%	a_mult_stats_db: A multi-page stats_db.
 %
 % See also: plot_abstract, plot_simple
 %
@@ -25,7 +24,10 @@ function a_mult_stats_db = compareStats(a_stats_db, varargin)
 % This work is licensed under the Academic Free License ("AFL")
 % v. 3.0. To view a copy of this license, please look at the COPYING
 % file distributed with this software or visit
-% http://opensource.org/licenses/afl-3.0.txt.
+% http://opensource.org/licenses/afl-3.0.php.
+
+% TODO: should take an array of stats_dbs rather than varargin, so that
+% additional arguments can be processed. Make sure to update user's manual.
 
 if ~ exist('props')
   props = struct([]);
