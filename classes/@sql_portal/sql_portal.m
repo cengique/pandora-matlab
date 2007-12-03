61d495e0b1063a888d533ab132905364d8621b69
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-12-03 23:34:39 +0000

    portal class to communicate with an sql server
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@862 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ classes/@sql_portal/sql_portal.m
@@ -0,0 +1,65 @@
+function obj = sql_portal(db_conn, id, props)
+
+% sql_portal - For import and export to external SQL engines using the Matlab Database Toolbox.
+%
+% Usage:
+% obj = sql_portal(db_conn, id, props)
+%
+% Description:
+%   Uses the Database (DB) Toolbox's connection object to read and write to
+% external SQL engines.
+%
+%   Parameters:
+%	db_conn: An object of the database class of the DB Toolbox.
+%	id: An identifying string.
+%	props: A structure with any optional properties.
+%		
+%   Returns a structure object with the following fields:
+%	db_conn: The DB Toolbox connection object.
+%	id, props.
+%
+% General operations on sql_portal objects:
+%   sql_portal	- Construct a new sql_portal object.
+%   tests_db	- Create a tests_db object from the result of an SQL query.
+%
+% Additional methods:
+%	See methods('sql_portal')
+%
+% See also: tests_db, database
+%
+% $Id: sql_portal.m 818 2007-08-28 20:28:51Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2007/11/29
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+if nargin == 0 %# Called with no params
+   obj.db_conn = [];
+   obj.id = '';
+   obj.props = struct([]);
+   obj = class(obj, 'sql_portal');
+ elseif isa(db_conn, 'sql_portal') %# copy constructor?
+   obj = db_conn;
+ else
+
+   % set some db parameters
+   logintimeout(5);
+  
+   % ping the connection to see if it's alive
+   ping(db_conn);
+   
+   if ~ exist('props')
+     props = struct([]);
+   end
+
+   obj.db_conn = db_conn;
+   obj.id = id;
+   obj.props = props;
+
+   obj = class(obj, 'sql_portal');
+end
+
---
+++ classes/@sql_portal/tests_db.m
@@ -0,0 +1,53 @@
+function a_db = tests_db(a_sql_portal, query_string, query_id, props)
+
+% tests_db - Create a tests_db object from the results of a SQL query.
+%
+% Usage:
+% a_db = tests_db(a_sql_portal, query_string, props)
+%
+% Description:
+%   Converter function to get a tests_db object properly annotated with
+% the metadata obtained from the results of the executed SQL
+% query. Currently this function is limited to importing numeric data only.
+%
+%   Parameters:
+%	a_sql_portal: A sql_portal object.
+%	query_string: An SQL query returning numeric results.
+%	query_id: Identifier associated witht the query, to be passed
+%		  into the tests_db object.
+%	props: A structure with any optional properties passed to tests_db.
+%		
+%   Returns:
+%	a_db: A tests_db object.
+%
+% See also: tests_db, database
+%
+% $Id: tests_db.m 818 2007-08-28 20:28:51Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2007/11/29
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+if ~exist('props', 'var')
+  props = struct;
+end
+
+% only numeric data
+setdbprefs('DataReturnFormat','numeric');
+
+% run query
+a_cursor = cursor(a_sql_portal.db_conn, query_string);
+
+% get the data
+a_cursor = fetch(a_cursor);
+
+% get names (only after fetching)
+attrs = attr(a_cursor);
+col_names = { attrs.fieldName };
+
+% create the tests_db object
+a_db = tests_db(a_cursor.Data, col_names, {}, query_id, props);
