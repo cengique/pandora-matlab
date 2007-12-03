f5fe89af3db2cc0055d8c3a1dbee6fcb4f4620d5
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-12-03 23:37:51 +0000

    debugged, optimized
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@867 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@plot_abstract/set.m
+++ classes/@plot_abstract/set.m
@@ -1,4 +1,4 @@
-function b = set(a, varargin)
+function a = set(a, varargin)
 % set - Generic method for setting object attributes.
 %
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
@@ -13,10 +13,9 @@ val = varargin{2};
 if ~ isa(a, 'plot_abstract')
   builtin('set', a, attr, val);
 else
-  eval(['a.' attr ' = val;']);
-  b = a;
+  a.(attr) = val;
 end
 % recurse
 if nargin > 3
-  b = set(a, varargin{3:end});
+  a = set(a, varargin{3:end});
 end
