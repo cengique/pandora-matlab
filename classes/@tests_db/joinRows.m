3c8cfeeb340b5ee6ac575b6746e8be886beaa39a
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-12-05 22:22:42 +0000

    major change to remove redundant input parameters and add a props param. Must change all references to it.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@872 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@tests_db/joinRows.m
+++ classes/@tests_db/joinRows.m
@@ -1,10 +1,9 @@
-function a_db = joinRows(db, tests, with_db, w_tests, index_col_name)
+function a_db = joinRows(a_db, with_db, props)
 
-% joinRows - Joins the rows of the given db with rows of with_db with matching
-%  	RowIndex values.
+% joinRows - Joins the rows of the given db with rows of with_db with matching RowIndex values.
 %
 % Usage:
-% a_db = joinRows(db, tests, with_db, w_tests, index_col_name)
+% a_db = joinRows(a_db, with_db, props)
 %
 % Description:
 %   Takes the desired columns in with_db with rows having a 
@@ -13,11 +12,10 @@ function a_db = joinRows(db, tests, with_db, w_tests, index_col_name)
 % db preserves the ordering of with_db.
 %
 %   Parameters:
-%	db: A param_tests_db object.
-%	tests: Test columns to take from db.
-%	with_db: A tests_db object with a RowIndex column.
-%	w_tests: Test columns to take from with_db.
-%	index_col_name: (Optional) Name of row index column (default='RowIndex').
+%	a_db: A tests_db object.
+%	with_db: A tests_db object with a row index column.
+%	props: A structure with any optional properties.
+%	  indexColName: (Optional) Name of row index column (default='RowIndex').
 %		
 %   Returns:
 %	a_db: A tests_db object.
@@ -34,10 +32,16 @@ function a_db = joinRows(db, tests, with_db, w_tests, index_col_name)
 % file distributed with this software or visit
 % http://opensource.org/licenses/afl-3.0.php.
 
-data = get(db, 'data');
+if ~ exist('props', 'var')
+  props = struct;
+end
+
+data = get(a_db, 'data');
 w_data = get(with_db, 'data');
 
-if ~ exist('index_col_name')
+if isfield(props, 'indexColName')
+  index_col_name = props.indexColName;
+else
   index_col_name = 'RowIndex';
 end
 
@@ -69,20 +73,18 @@ if any(isnan(joins))
   end
 end
 
-size_db = dbsize(db);
+% TODO: one can use addColumns instead
+size_db = dbsize(a_db);
 size_wdb = dbsize(with_db);
 
-cols = tests2cols(db, tests);
-w_cols = tests2cols(with_db, w_tests);
-
 new_size(1) = size_wdb(1);
-new_size(2) = length(cols) + length(w_cols); %# Except the page index
+new_size(2) = size_db(2) + size_wdb(2); %# Except the page index
 
-new_data = [ data(joins(~isnan(joins)), cols), w_data(~isnan(joins), w_cols) ];
+new_data = [ data(joins(~isnan(joins)), :), w_data(~isnan(joins), :) ];
 
 %# Get the column names straight
-cols_cell1 = fieldnames(get(db, 'col_idx'));
+cols_cell1 = fieldnames(get(a_db, 'col_idx'));
 cols_cell2 = fieldnames(get(with_db, 'col_idx'));
-a_db = set(db, 'data', new_data);
-a_db = set(a_db, 'col_idx', makeIdx({ cols_cell1{cols}, cols_cell2{w_cols} }));
-a_db = set(a_db, 'id', [ get(db, 'id') ' joined with ' get(with_db, 'id')]);
+a_db = set(a_db, 'data', new_data);
+a_db = set(a_db, 'col_idx', makeIdx({ cols_cell1{:}, cols_cell2{:} }));
+a_db = set(a_db, 'id', [ get(a_db, 'id') ' joined with ' get(with_db, 'id')]);
