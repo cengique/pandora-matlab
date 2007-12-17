c0d7f1396d5d3f9a20289a95b1be88e1eb179721
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2007-12-17 17:25:15 +0000

    new overloaded operation
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@888 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ classes/@tests_db/sqrt.m
@@ -0,0 +1,30 @@
+function a_db = sqrt(a_db)
+
+% sqrt - Takes the square root of a_db.
+%
+% Usage:
+% a_db = sqrt(a_db)
+%
+% Description:
+%  Overloaded sqrt function.
+%
+% Parameters:
+%   a_db: A tests_db.
+%		
+% Returns:
+%   a_db: The resulting tests_db.
+%
+% See also: sqrt
+%
+% $Id: sqrt.m 818 2007-08-28 20:28:51Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2007/12/13
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+a_db = set(a_db, 'id', [ 'sqrt(' get(a_db, 'id') ')' ]);
+a_db = tests_db(a_db, 'data', sqrt(get(a_db, 'data')));
