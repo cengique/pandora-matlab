7f008b2b523da00964d7210b9addd29c670d5959
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-01-09 22:37:26 +0000

    Added balancing inputs and max epochs for incr learning. May need to have balancing operate on tests_db.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@899 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@tests_db/approxMappingNNet.m
+++ classes/@tests_db/approxMappingNNet.m
@@ -12,7 +12,8 @@ function [an_approx_db, a_nnet] = approxMappingNNet(a_db, input_cols, output_col
 % algorithm (see newff). Returns and the trained network object and a
 % database with output columns obtained from the approximator. The outputs
 % can then be compared to the original database to test the success of the
-% approximation.
+% approximation. If 'warning on verbose' is issued prior to running, it
+% provides additional debug info.
 %
 %   Parameters:
 %	a_db: A tests_db object.
@@ -22,7 +23,10 @@ function [an_approx_db, a_nnet] = approxMappingNNet(a_db, input_cols, output_col
 %	  nnetFcn: Neural network classifier function (default='newff')
 %	  nnetParams: Cell array of parameters passed to nnetFcn after
 %	  	      inputs and outputs.
-%	  plotIt: If 1, plot training and classification results.
+%	  trainMode: 'batch' or 'incr'.
+%	  classProbs: 'prob': use probabilistic sampling to normalize
+%	  		prior class probabilities.
+%	  maxEpochs: maximum number of epochs to train for.
 %	  (Rest passed to tests_db)
 %		
 %   Returns:
@@ -47,6 +51,9 @@ function [an_approx_db, a_nnet] = approxMappingNNet(a_db, input_cols, output_col
 % file distributed with this software or visit
 % http://opensource.org/licenses/afl-3.0.php.
 
+vs = warning('query', 'verbose');
+verbose = strcmp(vs.state, 'on');
+
 if ~exist('props', 'var')
   props = struct;
 end
@@ -70,18 +77,52 @@ a_nnet = feval(props.nnetFcn, a_nnet_inputs, ...
                 a_nnet_ouputs, props.nnetParams{:});
 
 % set display params
-if ~ isfield(props, 'plotIt') || props.plotIt == 0
+if ~ verbose
   a_nnet.trainParam.show = NaN;
 end
 
+orig_inputs = a_nnet_inputs;
+% balance inputs, if requested
+if isfield(props, 'classProbs') && strcmp(props.classProbs, 'prob')
+  [a_nnet_inputs, a_nnet_ouputs] = ...
+      balanceInputProbs(a_nnet_inputs, a_nnet_ouputs, 1);
+end
+
 % train it
-a_nnet = train(a_nnet, a_nnet_inputs, a_nnet_ouputs);
+if ~ isfield(props, 'trainMode') || strcmp(props.trainMode, 'batch')
+  % batch training
+  a_nnet = train(a_nnet, a_nnet_inputs, a_nnet_ouputs);
+else
+  % incremental training
+  if isfield(props, 'maxEpochs')
+    num_passes = props.maxEpochs;
+  else
+    num_passes = 10;
+  end
+  goal_mse = 1e-3;
+  cell_ins = num2cell(a_nnet_inputs, 1);
+  cell_outs = num2cell(a_nnet_ouputs); 
+  last_conditions = [];
+  for pass_num = 1:num_passes
+    [a_nnet, nn_outs, nn_errs, last_conditions] = ...
+        adapt(a_nnet, cell_ins, cell_outs, last_conditions);
+    nn_mse = mse(nn_errs);
+    if verbose, nn_mse, end
+    if nn_mse < goal_mse
+      if verbose, disp([ 'mse goal ' num2str(goal_mse) ' met.']); end
+      break;
+    end
+  end
+end
+
 
 % return simulated approximator output in new db
 col_names = getColNames(a_db);
 an_approx_db = ...
     tests_db([get(onlyRowsTests(a_db, ':', input_cols), 'data'), ...
-              sim(a_nnet, a_nnet_inputs)'], ...
+              sim(a_nnet, orig_inputs)'], ...
              [ col_names(tests2cols(a_db, input_cols)), ...
                col_names(tests2cols(a_db, output_cols)) ], {}, ...
              [ 'NNet approximated ' get(a_db, 'id') ]);
+end
+
