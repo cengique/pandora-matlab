3adae24ddf1e60598701ff345d38da3818dfd429
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-01-10 05:13:26 +0000

    Generalized, takes optional props.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@921 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@physiol_bundle/physiol_bundle.m
+++ classes/@physiol_bundle/physiol_bundle.m
@@ -1,16 +1,18 @@
-function a_bundle = physiol_bundle(a_dataset, a_db, a_joined_db, props)
+function a_bundle = physiol_bundle(a_cell, props)
 
 % physiol_bundle - The physiology dataset and the DB created from it bundled together.
 %
 % Usage:
-% a_bundle = physiol_bundle(a_dataset, a_db, a_joined_db, props)
+% a_bundle = physiol_bundle(a_cell, props)
 %
 %   Parameters:
-%	a_dataset: A physiol_cip_traceset_fileset object.
+%	a_cell: A cell array that contains the following elements:
+%	a_dataset: A cell-enclosed physiol_cip_traceset_fileset object.
 %	a_db: The raw params_tests_db object created from the dataset. 
 %		It only needs to have the pAcip, pAbias, TracesetIndex, and ItemIndex columns.
 %	a_joined_db: The one-treatment-per-line DB created from the raw DB.
 %	props: A structure with any optional properties.
+%	  controlDB: Use this as the ontrol DB rather than computing.
 %		
 % Description:
 %   This is a subclass of dataset_db_bundle, specialized for physiology datasets. 
@@ -43,20 +45,29 @@ function a_bundle = physiol_bundle(a_dataset, a_db, a_joined_db, props)
 if nargin == 0 % Called with no params
   a_bundle.joined_control_db = params_tests_db;
   a_bundle = class(a_bundle, 'physiol_bundle', dataset_db_bundle);
-elseif isa(a_dataset, 'physiol_bundle') % copy constructor?
-  a_bundle = a_dataset;
+elseif isa(a_cell, 'physiol_bundle') % copy constructor?
+  a_bundle = a_cell;
 else
   if ~ exist('props')
     props = struct([]);
   end
 
-  a_bundle.joined_control_db = ...
-      a_joined_db(a_joined_db(:, {'TTX', 'Apamin', 'EBIO', ...
-				  'XE991', 'Cadmium', 'drug_4AP'}) == zeros(1, 6), :);
+  [a_dataset, a_db, a_joined_db] = deal(a_cell{:});
+  
+  if ~ isfield(props, 'controlDB')
+    a_bundle.joined_control_db = ...
+        a_joined_db(a_joined_db(:, {'TTX', 'Apamin', 'EBIO', ...
+                        'XE991', 'Cadmium', 'drug_4AP'}) == zeros(1, 6), ...
+                    :);
+  else
+    a_bundle.joined_control_db = props.controlDB;
+  end
+  
+  % TODO: do not remove anything from a_db, it's already stripped
+  % used to keep only: (:, {'pAcip', 'pAbias', 'TracesetIndex', 'ItemIndex'})
   a_bundle = ...
       class(a_bundle, 'physiol_bundle', ...
-	    dataset_db_bundle(a_dataset, ...
-			      a_db(:, {'pAcip', 'pAbias', 'TracesetIndex', 'ItemIndex'}), ...
+	    dataset_db_bundle(a_dataset, a_db, ...
 			      a_joined_db, props));
 end
 
