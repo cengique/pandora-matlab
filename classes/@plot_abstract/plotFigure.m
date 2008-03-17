332ecd670cc433b7a66db87a383a16fe4ad01eb6
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-03-17 21:23:45 +0000

    compiz-fusion workaround. may shift figures across workspaces, though!
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@996 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@plot_abstract/plotFigure.m
+++ classes/@plot_abstract/plotFigure.m
@@ -45,14 +45,20 @@ else
   title = [ get(a_plot, 'title') title_str ];
   set(handle, 'Name', title);
 
+  % wait a second for figure to materialize (workaround for
+  % Compiz-fusion)
+  a_timer = timer('StartDelay', .5, 'TimerFcn', '1');
+  start(a_timer); wait(a_timer);
+  
   if isfield(a_plot.props, 'PaperPosition')
     set(handle, 'PaperPosition', a_plot.props.PaperPosition);
-    old_units = get(handle, 'Units');
+    old_units = get(handle, 'Units')
     % Paper position is in inches
     set(handle, 'Units', 'inches');
-    old_pos = get(handle, 'Position');
+    old_pos = get(handle, 'Position')
     % get the width and height from the paper position
-    set(handle, 'Position', [ old_pos(1:2) a_plot.props.PaperPosition(3:4) ]);
+    set(handle, 'Position', ...
+                [ old_pos(1:2) a_plot.props.PaperPosition(3:4) ]);
     set(handle, 'Units', old_units);
   end
 
