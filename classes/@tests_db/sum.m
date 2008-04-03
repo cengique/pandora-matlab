1de85709dd2913aca6ee856a52b0fddc6c6af1d4
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-04-03 18:12:22 +0000

    now takes dim argument.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1009 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@tests_db/sum.m
+++ classes/@tests_db/sum.m
@@ -1,9 +1,9 @@
-function a_db = sum(a_db, props)
+function a_db = sum(a_db, dim, props)
 
 % sum - Creates a tests_db by summing all rows.
 %
 % Usage:
-% a_db = sum(a_db, props)
+% a_db = sum(a_db, dim, props)
 %
 % Description:
 %   Applies the sum function to whole DB. The resulting DB will have one row.
@@ -27,12 +27,63 @@ function a_db = sum(a_db, props)
 % file distributed with this software or visit
 % http://opensource.org/licenses/afl-3.0.php.
 
-% TODO: doesn't conform to the mean, std methods here. Make them work like this, too.
-
 if ~ exist('props')
   props = struct([]);
 end
 
+if ~ exist('dim')
+  dim = 1;                              % rows
+end
+
+% Always do row-wise
+order = 1:length(dbsize(a_db));
+if dim ~= 1
+  order(dim) = 1;
+  order(1) = dim;
+  data = permute(a_db.data, order);
+else
+  data = a_db.data;
+end
+
+% Allocate results array
+db_size = size(data);
+s = repmat(NaN, [1 db_size(2:end)]);
+
+[s, n] = recsum(data, length(db_size));
+
+if dim ~= 1
+  s = ipermute(s, order);
+end
+
+a_db = set(a_db, 'data', s);
+
 a_db = set(a_db, 'id', [ 'summed ' get(a_db, 'id') ]);
-a_db = set(a_db, 'row_idx', makeIdx({'sum'}));
-a_db = set(a_db, 'data', sum(get(a_db, 'data')));
+switch (dim)
+  case 1
+    a_db = set(a_db, 'row_idx', makeIdx({'sum'}));
+  case 2
+    a_db = set(a_db, 'col_idx', makeIdx({'sum'}))
+end
+
+% Recursive std needed for stripping NaNs in each dimension
+% TODO: taken from mean, generalize it!
+function [s, n] = recsum(data, dim)
+  if dim == 1
+    sdata = data(~isnan(data(:)) & ~isinf(data(:)));
+    n = size(sdata, 1);
+    if n == 0
+      % If a divide by zero error occured, 
+      % give it NaN value instead of an empty matrix.
+      s = NaN;
+    else
+      s = sum(sdata, 1);
+    end
+  else
+    for num=1:size(data, dim)
+      % Otherwise recurse
+      [dims{1:(dim-1)}] = deal(':');
+      dims{dim} = num;
+      [s(dims{:}) n(dims{:})] = recsum(data(dims{:}), dim - 1);
+    end
+  end
+
