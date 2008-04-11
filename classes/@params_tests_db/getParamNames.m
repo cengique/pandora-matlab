26a0a0665762de06c107ce37299250cc70e1a405
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-04-11 18:40:41 +0000

    convenience function.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1040 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ classes/@params_tests_db/getParamNames.m
@@ -0,0 +1,30 @@
+function col_names = getParamNames(db)
+
+% getParamNames - Gets parameter column names.
+%
+% Usage:
+% col_names = getParamNames(db)
+%
+% Description:
+%   Convenience function that delegates to getColNames.
+%
+% Parameters:
+%   db: A params_tests_db object.
+%		
+% Returns:
+%   col_names: A cell array of strings.
+%
+% See also: tests_db/getColNames, tests_db
+%
+% $Id: getParamNames.m 818 2007-08-28 20:28:51Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2008/04/03
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+col_names = ...
+    getColNames(onlyRowsTests(db, 1, 1:get(db, 'num_params')));
