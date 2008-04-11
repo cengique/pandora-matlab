3b9e7efe1cda4a0c8500256504cf737290dc7744
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-04-11 19:35:23 +0000

    Li Su convenience function, name and doc modified by CG.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1044 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ functions/defaultValue.m
@@ -0,0 +1,29 @@
+function value = defaultValue(varname, a_defaultvalue)
+
+% defaultValue - If variable unset, assign it a_defaultvalue.
+%
+% Usage:
+%   var = defaultValue(varname, a_defaultvalue)
+%
+% Description:
+%   If the variable has already been defined, it keeps unchanged. If the
+% variable doesn't exist or is an empty matrix, it will be assigned a
+% default value to it.
+%
+% Parameters: 
+%   varname: a string. the name of the variable.
+%   a_defaultvalue: value for the variable.
+% 
+% Example:
+% SamplingRate = defaultValue('SamplingRate', 10);
+%
+% Author: Li, Su
+%
+% Modified: Cengiz Gunay, 2008/04/11 - naturalized name and documentation
+% for Pandora.
+
+if evalin('caller', ['~exist(''' varname ''',''var'') || isempty(' varname ')'])
+    value=a_defaultvalue;
+else
+    value=evalin('caller', varname);
+end
