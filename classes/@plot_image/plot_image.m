6dd2e3d7ebd798e74b37dd8fa9baf4a0abdd8270
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-05-01 21:44:55 +0000

    new simplified plot_image class and modified dependencies.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1084 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@plot_image/plot_image.m
+++ classes/@plot_image/plot_image.m
@@ -1,31 +1,36 @@
-function a_plot = plot_image(image_data, a_colormap, num_colors, axis_labels, ...
+function a_plot = plot_image(image_data, axis_labels, ...
+                             colorbar_label, ...
                              title_str, props)
 
-% plot_image - Generic errorbar plot.
+% plot_image - Generic image plot.
 %
 % Usage:
-% a_plot = plot_image(image_data, a_colormap, num_colors, axis_labels, title_str, props)
+% a_plot = plot_image(image_data, axis_labels, colorbar_label, title_str, props)
 %
 % Description:
 %   Subclass of plot_abstract. The plot_abstract/plot command can be used to
 % plot this data. Needed to create this as a separate class to have the
-% axis ranges method implemented.
+% axis ranges method implemented and take advantage of plot_abstract props.
 %
 % Parameters:
-%	image_data: 2D matrix with image data.
-%	a_colormap: Colormap vector, function name or handle to colormap (e.g., 'jet').
-% 	num_colors: Parameter to be passed to the a_colormap.
-%	axis_labels: Cell array for X, Y axis labels.
-%	title_str: Plot description.
-%	props: A structure with any optional properties.
-%	  colorbar: If defined, show colorbar next to plot.
-%	  colorbarProps: Set colorbar axis properties.
-%	  colorbarLabel: Set colorbar y-axis label.
-%	  truncateDecDigits: Truncate labels to this many decimal digits.
-%	  maxValue: Maximal value at num_colors to annotate the colorbar.
-%	  reverseYaxis: If 1, display y-axis values in reverse (default=0).
-%	  (Rest passed to plot_abstract.)
-
+%   image_data: 2D matrix with image data.
+%   axis_labels: Cell array for X, Y axis labels.
+%   colorbar_label: String to appear next to colorbar.
+%   title_str: Plot description.
+%   props: A structure with any optional properties.
+%     colorbar: If defined, show colorbar next to plot.
+%     numGrads: Number of poles in the colormap gradient. If 1 (default),
+%	  it will be a monocolor gradient (e.g., gray-level); if 2, it will
+%	  be a dual-color gradient (e.g., blue to red) with a black
+%	  crossing point. This point is determined by the minValue
+%	  (below). Default numGrads=2, if negative values exist in
+%	  image_data after scaling.
+%     minValue,maxValue: Use these value to scale the data by 
+%	  (image_data - minValue) / (maxValue - minValue). 
+%	  Otherwise, its min and max is used.
+%     colormap: Colormap vector, function name or handle to colormap (e.g., 'jet').
+%     numColors: Number of colors in colormap.
+%     (Rest passed to plotImage.)
 %		
 %   Returns a structure object with the following fields:
 %	plot_abstract.
@@ -37,7 +42,7 @@ function a_plot = plot_image(image_data, a_colormap, num_colors, axis_labels, ..
 %	See methods('plot_image') and methods('plot_abstract')
 %
 % Example:
-% >> plotFigure(plot_image(rand(5) * 50, @jet, 50, {}, 'hello'))
+% >> plotFigure(plot_image(rand(5), {'r1', 'r2'}, 'rand', 'random matrix'))
 %
 % See also: plot_abstract, plot_abstract/plot
 %
@@ -60,13 +65,84 @@ elseif isa(image_data, 'plot_image') % copy constructor?
   a_plot = image_data;
 else
   if ~ exist('props')
-    props = struct([]);
+    props = struct;
+  end
+
+  if isfield(props, 'numColors')
+    num_colors = props.numColors;
+  else
+    num_colors = 50;
+  end
+
+  if isfield(props, 'colormap')
+    a_colormap = props.colormap;
+  else
+    a_colormap = [];
+  end
+
+  
+  if isfield(props, 'maxValue')
+    max_val = props.maxValue;
+  else
+    max_val = max(max(abs(image_data)));
+  end
+
+  if isfield(props, 'minValue')
+    min_val = props.minValue;
+  else
+    min_val = min(min(abs(image_data)));
+  end
+
+  image_data = (image_data - min_val) ./ (max_val - min_val) * num_colors;
+  is_symmetric = any(any(image_data < 0));
+
+  if isfield(props, 'numGrads')
+    num_grads = props.numGrads;
+  else
+    num_grads = 1;
+  end
+  
+  image_props = struct;
+  image_props.colorbarLabel = colorbar_label;
+  image_props.maxValue = max_val;
+    
+  % convert image_data to color values and handle colorbar
+  if is_symmetric || num_grads == 2
+    % symmetric
+    image_data = image_data + num_colors;
+    % first color for indicates blank for NaN values
+    if isempty(a_colormap)
+      a_colormap = [1 1 1; colormapBlueCrossRed(num_colors)];
+    end
+
+    image_props.colorbarProps = struct;
+    image_props.colorbarProps.YTick = ...
+        [2, num_colors + 1, 2*num_colors + 1];
+    if isfield(props, 'colorbarProps') && ...
+       isfield(props.colorbarProps, 'YTickLabel') 
+      image_props.colorbarProps.YTickLabel = ...
+          props.colorbarProps.YTickLabel;
+    else
+      image_props.colorbarProps.YTickLabel = ...
+          [ 2 * min_val - max_val, min_val, max_val ];
+    end
+  else
+    % monocolor gradient
+    if isempty(a_colormap)
+      a_colormap = gray(num_colors);
+    end
+    image_props.colorbarProps = struct;
+    image_props.colorbarProps.YTick = ...
+        [2:((num_colors - 2)/3):num_colors];
+    image_props.colorbarProps.YTickLabel = ...
+        image_props.colorbarProps.YTick ./ num_colors .* max_val;
   end
 
   a_plot = ...
       class(struct, 'plot_image', ...
             plot_abstract({image_data, ...
-                      a_colormap, num_colors, props}, ...
+                      a_colormap, num_colors, ...
+                      mergeStructs(props, image_props)}, ...
                           axis_labels, title_str, {}, @plotImage, props));
 end
 
--- classes/@tests_db/plotCovar.m
+++ classes/@tests_db/plotCovar.m
@@ -10,10 +10,13 @@ function a_plot = plotCovar(a_db, title_str, props)
 %   Parameters:
 %	a_db: A tests_db object that resulted from a function like cov.
 %	title_str: (Optional) String to append to plot title.
-%	props: Optional properties to be passed to plot_abstract.
+%	props: Optional properties.
 %	  inverse: If 1, take inverse of the data matrix.
-%	  crosscoef: If 1, normalize matrix elements as crosscoef would a cov matrix.
+%	  corrcoef: If 1, normalize matrix elements to get corrcoef values.
 %	  logScale: If 1, take logarithm of values before plotting.
+%	  localityIters: Apply a locality optimization algorithm with
+%	  	this many iterations.
+%	  (rest passed to plot_image.)
 %		
 %   Returns:
 %	a_plot: A plot_abstract object or one of its subclasses.
@@ -21,7 +24,7 @@ function a_plot = plotCovar(a_db, title_str, props)
 % Example:
 % >> plotFigure(plotCovar(cov(get(constrainedMeasuresPreset(pbundle2, 6), 'joined_control_db'))));
 %
-% See also: tests_db/cov, plotImage, tests_db/matchingRow.
+% See also: tests_db/cov, plotImage, tests_db/matchingRow, corrcoefs.
 %
 % $Id$
 %
@@ -40,17 +43,26 @@ end
 % TODO: put some sanity checks to make sure this DB is not too big to
 % plot
 
-image_data = a_db.data;
+image_data = a_db.data(:, :, 1);
+
+if isa(a_db, 'tests_3D_db')
+  data_label = fieldnames(get(a_db, 'page_idx'))
+  data_label = data_label{1};
+else
+  data_label = 'covariance';
+end
 
 % normalize
-if isfield(props, 'crosscoef')
+if isfield(props, 'corrcoef')
   selfcov_rows = diag(image_data) * ones(1, size(image_data, 2));
   image_data = image_data ./ sqrt(selfcov_rows .* selfcov_rows');
+  data_label = 'cross correlation';
 end
 
 % take inverse if requested
 if isfield(props, 'inverse')
   image_data = inv(image_data);
+  data_label = [ 'inverse ' data_label ];
 end
 
 % log scale
@@ -60,12 +72,13 @@ if isfield(props, 'logScale')
   log_cov_data(pos_data) = log(1 + image_data(pos_data));
   log_cov_data(~pos_data) = - log(1 - image_data(~pos_data));
   image_data = log_cov_data;
+  data_label = [ data_label ' in log-scale' ];
 end
 
 % optimize locality
 if isfield(props, 'localityIters')
   [image_data shuffle_idx] = ...
-      optimize_cov_matrix(image_data, props.localityIters)
+      optimize_cov_matrix(image_data, props.localityIters);
 else
   shuffle_idx = 1:size(image_data, 1);
 end
@@ -76,19 +89,16 @@ if ~ exist('title_str') || isempty(title_str)
   title_str = [properTeXLabel(get(a_db, 'id'))];
 end
 
-plot_props = struct('YTick', 1:dbsize(a_db, 1), 'border', [0.07 0 0 0], ...
-                    'colorbar', 1);
+plot_props = struct('YTick', 1:dbsize(a_db, 1), 'border', [0.07 0 0.03 0], ...
+                    'colorbar', 1, 'XTick', []);
 col_names = getColNames(a_db);
 plot_props.YTickLabel = properTeXLabel(col_names(shuffle_idx));
 
-% send this to the plot
-plot_props.maxValue = max(max(abs(image_data)));
+plot_props.truncateDecDigits = 2;
 
-a_plot = plot_abstract({image_data ./ plot_props.maxValue * num_colors + num_colors, ...
-                    @colormapBlueCrossRed, ...
-                    num_colors, plot_props}, [], ...
-		       title_str, {}, @plotImage, mergeStructs(props, ...
-                                                  plot_props))
+a_plot = ...
+    plot_image(image_data, {}, properTeXLabel(data_label), title_str, ...
+               mergeStructs(props, plot_props));
 end
 
 function [image_data, idx] = optimize_cov_matrix(image_data, num_iters)
@@ -122,6 +132,7 @@ end
 
 function entropy_val = entropy_cov_matrix(image_data)
 
-entropy_val = sum(sum(abs(diff(image_data, 1, 1))));
+diff_vals = diff(image_data, 1, 1);
+entropy_val = sum(sum(abs(diff_vals(~isnan(diff_vals)))));
 
 end
