a6caf9470d432ee10555ef554f6a53f7099e9d9b
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-05-27 20:21:57 +0000

    bugfix to keep parameters at start after joining.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1090 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@params_tests_db/joinRows.m
+++ classes/@params_tests_db/joinRows.m
@@ -38,7 +38,30 @@ if ~exist('props', 'var')
   props = struct;
 end
 
-% Call super class method
-a_db = params_tests_db(db.num_params, ...
-		       joinRows(db.tests_db, with_db, props), ...
+% delegate the main job to tests_db/joinRows
+joined_db = ...
+    joinRows(db.tests_db, with_db, props);
+
+% afterwards, calculate total parameter number and shift columns if
+% necessary
+all_param_names = getParamNames(db);
+
+% combine with params from with_db
+if isa(with_db, 'params_tests_db')
+  all_param_names = union(all_param_names, getParamNames(with_db));
+end
+
+% choose only params that appear in the output
+joined_col_names = getColNames(joined_db);
+output_param_names = intersect(all_param_names, joined_col_names);
+rest_col_names = setdiff(joined_col_names, output_param_names);
+num_params = length(output_param_names);
+
+% reorder to have params in the start
+joined_db = ...
+    onlyRowsTests(joined_db, ':', { output_param_names{:}, rest_col_names{:}});
+
+% construct new params_tests_db object with correct num_params
+a_db = params_tests_db(num_params, ...
+		       joined_db, ...
 		       get(db, 'props'));
