d0f38e59c3dae6e6d545e10494eea847c651fec9
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-05-27 20:25:29 +0000

    Returns a new trace which is the running average of the original trace.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1098 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ classes/@trace/runAvg.m
@@ -0,0 +1,30 @@
+function avg_t = runAvg(t)
+
+% runAvg - Returns a trace which is the running average of this trace.
+%
+% Usage:
+% avg_t = runAvg(t)
+%
+% Description:
+%
+%   Parameters:
+%	t: A trace object.
+%
+%   Returns:
+%	avg_t: A trace object that contains the running average of this trace.
+%
+% See also: trace
+%
+% $Id: runAvg.m 896 2007-12-17 18:48:55Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2008/05/14
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+dt = get(t, 'dt');
+data = get(t, 'data');
+avg_t = set(t, 'data', cumsum(data) ./ (1:length(data))' * dt);
