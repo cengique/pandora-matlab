17cd88dbbf90ae823d79d200cd88fb150714cd3f
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2008-10-16 18:34:15 +0000

    added test_suite dir
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1128 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ test_suite/tests_db/joinRowsUT.m
@@ -0,0 +1,34 @@
+function failed = joinRowsUT
+
+% joinRowsUT - Unit test for tests_db/joinRows
+%
+% Usage:
+% failed = joinRows
+%
+% Description:
+%
+% Parameters:
+%		
+% Returns:
+%   failed: Boolean.
+%
+% See also: 
+%
+% $Id: joinRows.m 989 2008-03-07 21:35:19Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/16
+
+a_db = tests_db([ (1:3)'], { 'Control' }, {}, 'test joinRows');
+
+order = [2; 1; 3];
+b_db = tests_db(order, {'RowIndex'}, {}, 'index joinRows');
+
+j_db = joinRows(a_db, b_db);
+
+failed = ~(j_db(:, 'Control') == order);
+
+if failed
+  disp('Failed test!');
+else
+  disp('Passed test!');
+end
