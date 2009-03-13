bbd074188612a4b59aa7338e211243e3ecf47fab
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2009-03-13 21:00:56 +0000

    added option to reverse sorting direction.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1167 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@tests_db/sortrows.m
+++ classes/@tests_db/sortrows.m
@@ -1,17 +1,19 @@
-function [sorted_db, idx] = sortrows(db, cols)
+function [sorted_db, idx] = sortrows(db, cols, props)
 
 % sortrows - Returns a sorted_db according to given columns. 
 %
 % Usage:
-% [sorted_db, idx] = sortrows(db, cols)
+% [sorted_db, idx] = sortrows(db, cols, props)
 %
 % Description:
 %   WARNING: For multi-page dbs, sorts only the first page and applies the ordering 
 % to all other pages which may produce wrong results for some applications.
 %
-%   Parameters:
-%	db: A tests_db object.
-%	cols: Columns to use for sorting.
+% Parameters:
+%   db: A tests_db object.
+%   cols: Columns to use for sorting.
+%   props: Structure of optional parameters.
+%	reverse: If exists, sort in reverse order.
 %		
 %   Returns:
 %	sorted_db: The sorted tests_db.
@@ -29,9 +31,14 @@ function [sorted_db, idx] = sortrows(db, cols)
 % file distributed with this software or visit
 % http://opensource.org/licenses/afl-3.0.php.
 
-if ~ exist('cols')
+if ~ exist('cols', 'var')
   cols = ':';
 end
+
+if ~ exist('props', 'var')
+  props = struct;
+end
+
 
 db_size = size(db.data);
 if length(db_size) > 2
@@ -48,6 +55,10 @@ if num_pages > 1
   % Sort the first page
   [db.data(:,:,1) idx] = sortrows(db.data(:,:,1), cols);
 
+  if isfield(props, 'reverse')
+    db.data = db.data(end:-1:1, :, 1);
+    idx = idx(end:-1:1);
+  end
 
   % Sort each page with the first page idx
   for page_num=2:num_pages
@@ -56,6 +67,11 @@ if num_pages > 1
 else
   % Sort all
   [db.data idx] = sortrows(db.data, cols);
+  
+  if isfield(props, 'reverse')
+    db.data = db.data(end:-1:1, :);
+    idx = idx(end:-1:1, :);
+  end
 end
 
 % Reorder row_idx if it exists
