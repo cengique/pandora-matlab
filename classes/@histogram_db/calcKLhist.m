b079ec4643f9e4f26e43e8f8289ce9498f9b5b45
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2009-04-06 03:40:04 +0000

    improved KL divergence calculation. added EPS to histograms to avoid zeros.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1177 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@histogram_db/calcKLhist.m
+++ classes/@histogram_db/calcKLhist.m
@@ -1,21 +1,29 @@
-function kl_bits = calcKLhist(a_hist_db, a2_hist_db, props)
+function kl_bits = calcKLhist(two_hist_dbs, props)
 
 % calcKLhist - Calculates the Kullback-Leibler divergence between two histograms with same bins.
 %
 % Usage:
-% kl_bits = calcKLhist(a_hist_db, a2_hist_db, props)
+% kl_bits = calcKLhist(two_hist_dbs, props)
 %
 % Parameters:
-%   a_hist_db, a2_hist_db: histogram_db objects to calculate KL divergence.
+%   two_hist_dbs: Array of two histogram_db objects to calculate KL divergence.
 %   props: Structure with optional parameters.
+%     sym: calculate symmetric KL divergence. Can be 'sum' for sum of
+%          divergences, 'avg' for  average, and 'res' for resistor average.
 %		
 % Returns:
 %   kl_bits: The calculated non-negative divergence in bits.
 %
 % Description:
-%   Histograms must have same bins!
+%   Histograms must have same bins! See example how to use
+% tests_db/histogram to get same bins from multiple DBs.
 %
-% See also: histogram_db, calcKLmodel
+% Example:
+% % Find 100-bin histograms of column var1 from two DBs and calculate
+% % their KL divergence
+% >> kl_bits = calcKLhist(histogram([one_db, another_db], 'var1', 100))
+%
+% See also: histogram_db, calcKLmodel, tests_db/histogram
 %
 % $Id: calcKLhist.m 896 2007-12-17 18:48:55Z cengiz $
 %
@@ -31,14 +39,31 @@ if ~ exist('props', 'var')
   props = struct;
 end
 
-data = get(a_hist_db, 'data');
-data2 = get(a2_hist_db, 'data');
+if ~ isa(two_hist_dbs, 'histogram_db') || length(two_hist_dbs) ~= 2
+  error('Argument two_hist_dbs must be an array with two elements of type histogram_db.');
+end
+
+data = get(two_hist_dbs(1), 'data');
+data2 = get(two_hist_dbs(2), 'data');
 
 % Normalize histogram to simulate a probability distribution function
-% (PDF)
-data = data(:, 2) / sum(data(:, 2));
-data2 = data2(:, 2) / sum(data2(:, 2));
-
+% (PDF) and make zero bins small numbers (EPS) for log calculations
+data = data(:, 2) / sum(data(:, 2)) + eps;
+data2 = data2(:, 2) / sum(data2(:, 2)) + eps;
 
 % Calculate KL divergence
 kl_bits = calcKL(data, data2);
+
+if isfield(props, 'sym')
+  kl_bits_right = calcKL(data2, data);
+  switch props.sym
+    case 'sum'
+      kl_bits = kl_bits + kl_bits_right;
+    case 'avg'
+      kl_bits = (kl_bits + kl_bits_right)/2;
+    case 'res'
+      kl_bits = (kl_bits * kl_bits_right) / (kl_bits + kl_bits_right);
+    otherwise
+      error(['Property "sym" has unrecognized value: "' props.sym '"']);
+  end
+end
