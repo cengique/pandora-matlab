47c557de9645163dd994d0e8c70118f6caebb39d
Cengiz Gunay <cengique@users.sf.net>
2010-01-21 23:42:46 +0000

    Major improvements: - new convenience functions - control of tuned parameters - can compose new functions from existing ones TODO: - control and set bounds - implement fitting functions in here

---
+++ param_func_nil.m
@@ -0,0 +1,44 @@
+function a_pf = param_func_nil(value, id, props)
+  
+% param_func_nil - A null function, returns constant value.
+%
+% Usage:
+%   a_pf = param_func_nil(value, id, props)
+%
+% Parameters:
+%   value: Constant value to return.
+%   id: An identifying string for this function.
+%   props: A structure with any optional properties.
+% 	   (Rest passed to param_func)
+%		
+% Returns: 
+%   a_pf: A a param_func object that always returns a constant value.
+%
+% Description:
+%
+% Example:
+% >> m_ClCa = param_func_nil(1, 'm_ClCa');
+%
+% See also: param_func
+%
+% $Id: param_func_nil.m 1174 2009-03-31 03:14:21Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2010/01/17
+
+  if ~ exist('props', 'var')
+    props = struct;
+  end
+
+  if ~ exist('value', 'var')
+    value = 0;
+  end
+  
+  if ~ exist('id', 'var')
+    id = ['Constant (' num2str(value) ') function'];
+  end
+  
+  a_pf = ...
+    param_func({'undefined', 'constant'}, [], {}, ...
+               @(p, c) deal(value, NaN), ...
+               id, props);
+end
