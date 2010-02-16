313020086a301bb2f770bd0f64777e30895f2cbe
Cengiz Gunay <cengique@users.sf.net>
2010-02-16 17:06:15 +0000

    added voltage-clamp object and latest matlab trials
    git-svn-id: file:///home/cengiz/stg-svnreps/flymotor/trunk/voltage-clamp@61 56a705e8-4ec7-11de-8a84-d56a39cf5c17

---
+++ @voltage_clamp/get.m
@@ -0,0 +1,29 @@
+function b = get(a, attr)
+% get - Defines generic attribute retrieval for objects.
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/14
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+% If input is an array, then also return array
+num_items = length(a);
+if num_items > 1 
+  % Create array of outputs
+  for item_num = 1:num_items
+    b(item_num) = get(a(item_num), attr);
+  end
+  return;
+end
+
+try
+  b = getfield(struct(a), attr);
+catch
+  errstr = lasterror;
+
+  % Then try the parent class
+  b = get(a.trace, attr);
+end
---
+++ @voltage_clamp/voltage_clamp.m
@@ -0,0 +1,65 @@
+function a_vc = voltage_clamp(data_i, data_v, dt, di, dv, id, props)
+
+% voltage_clamp - Voltage clamp object with current and voltage traces.
+%
+% Usage:
+% a_vc = voltage_clamp(datasrc, dt, dy,
+%		  pulse_time_start, pulse_time_width, id, props)
+%
+% Description:
+%		
+% Uses the generic trace object to store an averaged spike shape. 
+% Inherits the common methods defined in trace.
+%
+%   Parameters:
+%	datasrc: A vector of data points containing the spike shape.
+%	dt: Time resolution [s].
+%	dy: y-axis resolution [ISI (V, A, etc.)]
+%	pulse_time_start, pulse_time_width:
+%		Start and width of the pulse [dt]
+%	id: Identification string.
+%	props: A structure with any optional properties, such as:
+%		trace_time_start: Samples in the beginning to discard [dt]
+%		(see trace for more)
+%
+% Returns a structure object with the following fields:
+%   v: Voltage trace object, 
+%   i: Current trace object,
+%   trace: A parent trace object to inherit methods from.
+%
+% General methods of voltage_clamp objects:
+%   voltage_clamp		- Construct a new voltage_clamp object.
+%
+% Additional methods:
+%   See methods('voltage_clamp')
+%
+% See also: trace, period
+%
+% $Id: voltage_clamp.m 1174 2009-03-31 03:14:21Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2010/02/05
+
+% Copyright (c) 2007-2010 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+if nargin == 0 % Called with no params
+  a_vc = struct;
+  a_vc.i = trace;
+  a_vc.v = trace;
+  a_vc = class(a_vc, 'voltage_clamp', trace);
+elseif isa(data_i, 'voltage_clamp') % copy constructor?
+  a_vc = data_i;
+else
+  if ~ exist('props', 'var')
+    props = struct;
+  end
+
+  a_vc = struct;
+  a_vc.i = trace(data_i, dt, di, [ id ', I' ], props);
+  a_vc.v = trace(data_v, dt, dv, [ id ', V' ], props);
+
+  a_vc = class(a_vc, 'voltage_clamp', trace([], dt, NaN, id, props));
+end
