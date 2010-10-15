26041968584581e86694f6402b4e81fe49acce47
Cengiz Gunay <cengique@users.sf.net>
2010-10-15 19:03:04 +0000

    Use imported Neurofit parameters to make a param_I_int_v object. Already tested and negative tau bugs were fixed.

---
+++ param_I_Neurofit.m
@@ -0,0 +1,67 @@
+function a_pf = ...
+      param_I_Neurofit(neurofit_params, id, props)
+  
+% param_I_Neurofit - Create I channel from Neurofit parameters using param_I_int_v.
+%
+% Usage:
+%   a_pf = 
+%     param_I_Neurofit(neurofit_params, id, props)
+%
+% Parameters:
+%   neurofit_params: Structure coming from paramsNeurofit function.
+%   id: An identifying string for this function.
+%   props: A structure with any optional properties.
+%     label: Short name to use in channel description
+%     (Rest passed to param_I_int_v)
+%		
+% Returns a structure object with the following fields:
+%	a_pf: Holds the voltage->current function.
+%
+% Description:
+%   Uses splines to make taus as continuous functions of voltage. Dummy
+%   tau values are added at -100 and 100 mV using last known taus. WARNING:
+% assumes units are in: gmax [nS], tau [ms] and V [mV].
+%
+% Example:
+% >> f_Na_v = param_I_Neurofit(paramsNeurofit('Na_chan_fits_Report.txt'), 'I_Na');
+%
+% See also: param_I_int_v, param_func
+%
+% $Id: param_I_Neurofit.m 128 2010-06-07 21:36:08Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2010/10/13
+
+props = defaultValue('props', struct);
+label = getFieldDefault(props, 'label', '');
+if ~ isempty(label)
+  label_comma = [ label ', ' ];
+else
+  label_comma = '';
+end
+
+minf_v = ...
+    param_act([neurofit_params.V2m neurofit_params.sm], [ 'm_{' label_comma '\infty}' ]);
+
+mtau_v = ...
+    param_tau_spline_v([-100, neurofit_params.tau_m(1, :), 100], ...
+                       [neurofit_params.tau_m(2, 1), neurofit_params.tau_m(2, :), neurofit_params.tau_m(2, end)], ...
+                       [ '\tau_{' label_comma 'm}' ]); 
+
+m_d = param_act_deriv_v(minf_v, mtau_v, [ 'm_{' label '}' ], ...
+                           struct('name', 'm'));
+
+hinf_v = ...
+    param_act([neurofit_params.V2h neurofit_params.sh], [ 'h_{' label_comma '\infty}' ]);
+
+htau_v = ...
+    param_tau_spline_v([-100, neurofit_params.tau_h1(1, :), 100], ...
+                       [neurofit_params.tau_h1(2, 1), neurofit_params.tau_h1(2, :), neurofit_params.tau_h1(2, end)], ...
+                       [ '\tau_{' label_comma 'h}' ]); 
+
+h_d = param_act_deriv_v(hinf_v, htau_v, [ 'h_{' label '}' ], ...
+                           struct('name', 'h'));
+
+a_pf = ...
+    param_I_int_v([neurofit_params.p neurofit_params.q ...
+                   neurofit_params.gmax*1e-3 neurofit_params.Erev], m_d, h_d, ...
+                  ['I_{' label '}'], props);
