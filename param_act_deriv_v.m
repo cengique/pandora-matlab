533baa488d9db0a26137e4c1b39a7953fd5ec7e3
Cengiz Gunay <cengique@users.sf.net>
2010-10-29 23:13:35 +0000

    working on improving derivatives. not done yet.

--- param_act_deriv_v.m
+++ param_act_deriv_v.m
@@ -6,7 +6,7 @@ function a_pf = param_act_deriv_v(ap_inf_v, ap_tau_v, id, props)
 %   a_pf = param_act_deriv_v(ap_inf_v, ap_tau_v, id, props)
 %
 % Parameters:
-%   ap_inf_v, ap_tau_v: param_act objects for inf(v) and tau(v), resp.
+%   ap_inf_v, ap_tau_v: param_act objects for inf(v) and tau(v) in [ms], resp.
 %   id: An identifying string for this function.
 %   props: A structure with any optional properties.
 % 	   (Rest passed to param_func)
@@ -29,13 +29,16 @@ function a_pf = param_act_deriv_v(ap_inf_v, ap_tau_v, id, props)
 % TODO:
 % - allow setting initial value rather than taking it from v(1)
 
-  if ~ exist('props', 'var')
-    props = struct;
-  end
+  props = defaultValue('props', struct);
+  id = defaultValue('id', '');
 
   % for inner function
   name = getFieldDefault(props, 'name', id);
 
+  if isempty(name), name = 'm'; end
+  
+  props.name = name;
+  
   a_pf = ...
       param_mult(...
         {'time [ms]', 'activation'}, ...
@@ -46,14 +49,27 @@ function a_pf = param_act_deriv_v(ap_inf_v, ap_tau_v, id, props)
                                                     'init_val_func', ...
                                                     @(fs, v) f(fs.inf, v))));
 
+% TODO: this should be an object of deriv_func, which should be a
+% subclass of param_mult, where the f function is overloaded to do the
+% integration.
+
+% TODO: simplify this function, running too slow again!
 
   function dact = act_func_deriv(fs, p, x)
-    t = x.t;
-    s = x.s;
+    s = getFieldDefault(x, 's', []);
     v = x.v;
+    t = getFieldDefault(x, 't', 0);
     dt = x.dt;
-    %v_ind = 1; %v(round(t/dt)+1, :)';
-    dact = ((f(fs.inf, v) - getVal(s, name)) ./ ...
-            f(fs.tau, v));
+    if isempty(s)
+      s = solver_int({}, dt, [ 'solver for ' id ] );
+      s = initSolver(fs.this, s, struct('initV', v(1)));
+      var_int = integrate(s, v, mergeStructs(get(fs.this, 'props'), props));
+      % return integrated variable
+      dact = squeeze(var_int(:, 1, :));
+    else
+      % return derivative
+      dact = (f(fs.inf, v) - getVal(s, name)) ./ ...
+              f(fs.tau, v); % TODO: convert to [s] => WRONG!!!
+    end
   end
 end
