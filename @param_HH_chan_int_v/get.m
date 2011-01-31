200202a9bd16482d965de1b7440f35784c728d1f
Cengiz Gunay <cengique@users.sf.net>
2011-01-31 17:50:40 +0000

    new HH channel class supercedes param_I_int_v

---
+++ @param_HH_chan_int_v/get.m
@@ -0,0 +1,15 @@
+function b = get(a, attr)
+% get - Defines generic attribute retrieval for objects.
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/14
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+if isfield(struct(a), attr)
+  b = a.(attr);
+else
+  b = get(a.param_mult, attr);
+end
