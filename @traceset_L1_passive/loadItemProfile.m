e70b59a67d924296b763db4bf4d6b7af931f9907
Cengiz Gunay <cengique@users.sf.net>
2011-02-03 21:32:00 +0000

    first automated db construction for passive params
    git-svn-id: file:///home/cengiz/stg-svnreps/flymotor/trunk/voltage-clamp@335 56a705e8-4ec7-11de-8a84-d56a39cf5c17

---
+++ @traceset_L1_passive/loadItemProfile.m
@@ -0,0 +1,53 @@
+function [a_profile a_doc] = loadItemProfile(traceset, trace_index)
+
+% loadItemProfile - Loads a cip_trace_profile object from a raw data file in the traceset.
+%
+% Usage:
+% a_profile = loadItemProfile(traceset, trace_index)
+%
+% Description:
+%
+%   Parameters:
+%	traceset: A physiol_cip_traceset object.
+%	trace_index: Index of file in traceset.
+%		
+%   Returns:
+%	a_profile: A profile object that implements the getResults method.
+%
+% See also: itemResultsRow, params_tests_fileset, paramNames, testNames
+%
+% $Id: loadItemProfile.m 896 2007-12-17 18:48:55Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/09/14
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+traceset_props = get(traceset, 'props');
+
+basedir = getFieldDefault(traceset_props, 'baseDir', '');
+
+a_trace = getItem(traceset, trace_index);
+
+% use the template if provided
+if isfield(traceset_props, 'fileTempl')
+  a_trace = sprintf(traceset_props.fileTempl, a_trace);
+end
+
+% load the voltage_clamp object
+if ischar(a_trace)
+  a_trace = abf2voltage_clamp([ basedir a_trace ], ...
+                              [ '-' num2str(trace_index) ], traceset_props);
+end
+
+assert(isa(a_trace, 'voltage_clamp'));
+
+% get the results
+[a_profile a_doc] = getResultsPassiveReCeElec(data_L1_passive(a_trace), traceset_props);
+
+% pack it into a cell
+a_profile = {results_profile(a_profile, [get(traceset, 'id') ' - Item=' ...
+                    num2str(trace_index) ]) a_doc};
---
+++ @traceset_L1_passive/traceset_L1_passive.m
@@ -0,0 +1,69 @@
+function a_tset = traceset_L1_passive(traces_cell, treatments, neuron_id, props)
+
+% traceset_L1_passive - Dataset of multiple ABF files that belong to the same cell.
+%
+% Usage:
+% a_tset = traceset_L1_passive(traces_cell, treatments, neuron_id, props)
+%
+% Parameters:
+%   traces_cell: Cell array of file names or voltage_clamp objects.
+%   treatments: Structure with treatments/mutations and values.
+%   neuron_id: Neuron name.
+%   props: A structure with any optional properties.
+%     fileTempl: A sprintf template to place the items in traces_cell to
+%     		 generate the final filename. See example below.
+%     baseDir: Directory to find the files.
+%     (All other props are passed to getResultsPassiveReCeElec in loadItemProfile)
+%		
+%   Returns a structure object with the following fields:
+%	params_tests_dataset,
+%	treatments, neuron_id.
+%
+% Description:
+%   This is a subclass of params_tests_dataset. 
+%
+% Example:
+% > a_ts = traceset_L1_passive({1, 5, 456}, 'my neuron',
+% 				struct('fileTempl', 'file%05d.abf',
+% 				'baseDir', 'C:/myfiles/'));
+% would make a traceset out of file00001.abf, file00005 and file00456.abf.
+%
+% General operations on traceset_L1_passive objects:
+%   traceset_L1_passive - Construct a new object.
+%   loadItemProfile	- Builds a data_L1_passive for each trace in the set.
+%
+% Additional methods:
+%	See methods('traceset_L1_passive'), and 
+%	    methods('params_tests_dataset').
+%
+% See also: sprintf, cip_traces, params_tests_dataset, params_tests_db
+%
+% $Id: traceset_L1_passive.m 1174 2009-03-31 03:14:21Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2011/02/02
+
+% Copyright (c) 2011 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+vs = warning('query', 'verbose');
+verbose = strcmp(vs.state, 'on');
+
+if nargin == 0 % Called with no params
+  a_tset.treatments = struct;
+  a_tset.neuron_id = '';
+  a_tset = class(a_tset, 'traceset_L1_passive', params_tests_dataset);
+elseif isa(traces_cell, 'traceset_L1_passive') % copy constructor?
+  a_tset = traces_cell;
+else
+  a_tset.treatments = treatments;
+  a_tset.neuron_id = neuron_id;
+  
+  % Create the object 
+  a_tset = class(a_tset, 'traceset_L1_passive', ...
+                 params_tests_dataset(traces_cell, [], [], ...
+                                      neuron_id, props));
+end
+
