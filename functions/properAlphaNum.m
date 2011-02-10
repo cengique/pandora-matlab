3d3abaa8a95695c14f7b1a0cf833a5df7e91dc47
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2011-02-10 17:24:48 +0000

    only pass alphanumeric text
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1220 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ functions/properAlphaNum.m
@@ -0,0 +1,31 @@
+function a_label = properAlphaNum(a_label)
+
+%  properAlphaNum - Replaces characters in string to make it only alphanumeric.
+%
+% Usage:
+% a_label = properAlphaNum( a_label )
+%
+% Parameters:
+%   a_label: A label string.
+%
+% Returns:
+%   a_label: The corrected proper a_label.
+%
+% Description:
+%   It will only keep the character set 'A-Z a-z 0-9 _'
+%
+% Example: 
+% >> a_label = properAlphaNum('to \this _day+1 and ^5')
+% ans = 'tothis_day1and5' 
+%
+% $Id: properAlphaNum.m,v 1.2 2006/08/11 16:53:45 cengiz Exp $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2011/01/19
+
+% Copyright (c) 20011 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+a_label = regexprep(a_label, '[^A-Za-z0-9_]', '');
