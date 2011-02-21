1a067a8534fa22cab18052f123d3e500eeb7117b
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2011-02-21 23:10:41 +0000

    convert period to string
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1223 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

---
+++ classes/@period/char.m
@@ -0,0 +1,15 @@
+function s = char(t)
+
+% Generic object char method.
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/08/04
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+
+s = sprintf('[%d %d]', t.start_time, t.end_time);
+
+
