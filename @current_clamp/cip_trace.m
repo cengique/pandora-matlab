1a0df4f7e08e4e3dfe0cbc568808dc0eb2eb3710
Cengiz Gunay <cengique@users.sf.net>
2011-03-04 21:17:51 +0000

    addec convenience function cip_trace
    git-svn-id: file:///home/cengiz/stg-svnreps/flymotor/trunk/voltage-clamp@391 56a705e8-4ec7-11de-8a84-d56a39cf5c17

--- @current_clamp/cip_trace.m
+++ @current_clamp/cip_trace.m
@@ -1,17 +1,19 @@
-function [results profs] = getResults(a_cc, props)
+function [a_ct cip_level_pA] = cip_trace(a_cc, cip_num, props)
 
-% getResults - Extract measurement results.
+% cip_trace - Return a cip_trace object of the desired current step.
 %
 % Usage:
-% results = getResults(a_cc)
+% a_ct = cip_trace(a_cc, cip_num, props)
 %
 % Parameters:
 %   a_cc: A cip_trace object.
+%   cip_num: Index of CIP level.
 %   props: A structure with any optional properties.
 %     stepNum: Current step to get results for (default=2).
 %
 % Returns:
-%   results: A structure associating test names with result values.
+%   a_ct: A cip_trace object with voltage.
+%   cip_level_pA: applied current magnitude [pA].
 %
 % Description:
 %
@@ -33,26 +35,21 @@ props = ...
 
 step_num = getFieldDefault(props, 'stepNum', 2);
 
-% go through i_steps and collect cip_trace results
 a_vc = a_cc.voltage_clamp;
 
 % make a cip_trace object
 % use custom filter by default since most data won't be sampled at 10 kHz
+istep_num = cip_num;
+a_ct = ...
+    cip_trace(a_vc.v.data, a_vc.v.dt, ...
+              a_vc.v.dy, ...
+              a_vc.time_steps(1), ...
+              diff(a_vc.time_steps(1:2)), ...
+              a_vc.id, ...
+              mergeStructs(props, ...
+                           struct('custom_filter', 1, ...
+                                  'channel', istep_num)));
 
-% TODO: shouldn't istep value be a DB param?
-
-num_isteps = size(a_vc.i_steps, 2);
-profs = cell(1, num_isteps);
-for istep_num = 1:num_isteps  
-  a_results = struct;
-  % make current step a parameter
-  [a_ct a_results.cip_level_pA] = ...
-      cip_trace(a_cc, istep_num, props);
-
-  profs{istep_num} = getProfileAllSpikes(a_ct);
-  a_results = ...
-      mergeStructs(a_results, ...
-                   getResults(profs{istep_num}));
-  results = defaultValue('results', repmat(a_results, 1, num_isteps));
-  results(istep_num) = a_results;
-end
+% make current step a parameter
+cip_level_pA = ...
+      1e3 * a_vc.i_steps(step_num, istep_num);
