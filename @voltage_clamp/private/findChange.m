0e010544ffa3fc9fa250cc6a3f6d6f6d43ec04fd
Cengiz Gunay <cengique@users.sf.net>
2011-03-17 17:50:11 +0000

    fix real valued indices
    git-svn-id: file:///home/cengiz/stg-svnreps/flymotor/trunk/voltage-clamp@412 56a705e8-4ec7-11de-8a84-d56a39cf5c17

--- @voltage_clamp/private/findChange.m
+++ @voltage_clamp/private/findChange.m
@@ -1,9 +1,10 @@
 function t_change = find_change(data, idx_start, num_mV, dt)
 % find starting baseline
+  idx_start = round(idx_start);
   first_ms = 2;
   t_begin = first_ms/dt;
-  v_start = mean(data(idx_start:(idx_start + t_begin)));
-  %v_start_sd = std(data(idx_start:(idx_start + t_begin)));
+  v_start = mean(data(idx_start:round(idx_start + t_begin)));
+  %v_start_sd = std(data(idx_start:round(idx_start + t_begin)));
   
   % find beginning of step (used to be: 5*v_start_sd)
   t_change = find(abs(data(idx_start:end) - v_start) > num_mV); 
