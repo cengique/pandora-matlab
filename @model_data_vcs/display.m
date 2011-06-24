bc164fe20d85734126f8ba0cf71e68abbdffe3fe
Cengiz Gunay <cengique@users.sf.net>
2011-06-24 18:45:09 +0000

    generic display func
    git-svn-id: file:///home/cengiz/stg-svnreps/flymotor/trunk/voltage-clamp@498 56a705e8-4ec7-11de-8a84-d56a39cf5c17

---
+++ @model_data_vcs/display.m
@@ -0,0 +1,32 @@
+function str = display(a_md)
+
+% display - Return string representation of object.
+%
+% Usage:
+% str = display(a_md)
+%
+% Parameters:
+%   a_md: A model_data_vcs object.
+% 
+% Returns:
+%   str: Readable string
+%
+% Description:
+%
+% Example:
+%
+% See also: model_data_vcs
+%
+% $Id: display.m 238 2010-10-22 22:41:07Z cengiz $
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2011/05/31
+
+% Handle differently if an array
+if length(a_md) > 1
+  disp(a_md);
+  return;
+end
+
+%props = get(a_md, 'props');
+
+disp(sprintf('%s, %s', class(a_md), get(a_md, 'id')));
