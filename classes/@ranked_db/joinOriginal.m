7c319be1cc44f8bd11a4859ffc5e6c834880268f
cengiz <cengiz@bfe95d43-7c35-0410-b79e-65a0a38dd5e9>
2014-05-22 22:40:11 +0000

    added prop to keep item indices.
    git-svn-id: svn+ssh://cengique.2y.net/home/cengiz/stg-svnreps/pandora/pandora/trunk@1416 bfe95d43-7c35-0410-b79e-65a0a38dd5e9

--- classes/@ranked_db/joinOriginal.m
+++ classes/@ranked_db/joinOriginal.m
@@ -1,20 +1,22 @@
-function a_db = joinOriginal(a_ranked_db, rows)
+function a_db = joinOriginal(a_ranked_db, rows, props)
 
 % joinOriginal - Joins the distance values to the original db rows with matching row indices.
 %
 % Usage:
-% a_db = joinOriginal(a_ranked_db, rows)
+% a_db = joinOriginal(a_ranked_db, rows, props)
+%
+% Parameters:
+%   a_ranked_db: A ranked_db object.
+%   rows: Join only the given rows.
+%   props: A structure with any optional properties.
+%     includeIndices: Also joins ItemIndex columns from the original db.
+%		
+% Returns:
+%   a_db: A params_tests_db object (same type as a_ranked_db.orig_db) containing 
+%	the desired rows in ascending order of distance.
 %
 % Description:
 %   Takes the parameter columns from orig_db and all tests from crit_db.
-%
-%   Parameters:
-%	a_ranked_db: A ranked_db object.
-%	rows: Join only the given rows.
-%		
-%   Returns:
-%	a_db: A params_tests_db object (same type as a_ranked_db.orig_db) containing 
-%		the desired rows in ascending order of distance.
 %
 % See also: tests_db
 %
@@ -22,11 +24,13 @@ function a_db = joinOriginal(a_ranked_db, rows)
 %
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2004/12/21
 
-% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% Copyright (c) 2007-14 Cengiz Gunay <cengique@users.sf.net>.
 % This work is licensed under the Academic Free License ("AFL")
 % v. 3.0. To view a copy of this license, please look at the COPYING
 % file distributed with this software or visit
 % http://opensource.org/licenses/afl-3.0.php.
+
+props = defaultValue('props', struct);
 
 if ~exist('rows', 'var')
   rows = ':';
@@ -52,9 +56,13 @@ crit_cols = {crit_cols{all_test_cols}};
 % only keep columns in db
 crit_cols = intersect(crit_cols, getColNames(a_ranked_db));
 
-db_cols = fieldnames(get(a_ranked_db.orig_db, 'col_idx'));
+orig_cols = getParamNames(a_ranked_db.orig_db);
+if isfield(props, 'includeIndices')
+  orig_cols = [ orig_cols, {'/ItemIndex/'} ];
+end
+
 a_db = joinRows(onlyRowsTests(a_ranked_db.orig_db, ':', ...
-                              {db_cols{1:a_ranked_db.orig_db.num_params}, ...
+                              {orig_cols{:}, ...
                     crit_cols{:}}), ...
 		onlyRowsTests(a_ranked_db, rows, {'Distance', 'RowIndex'}));
 
