c8af937fd6755f8ec609d1c89bb282feec91e574
Cengiz Gunay <cengique@users.sf.net>
2015-01-30 18:20:40 -0500

    untested changes to generalize for all colormap plots.

--- functions/plotImage.m
+++ functions/plotImage.m
@@ -1,40 +1,52 @@
-function h = plotImage(image_data, a_colormap, num_colors, props)
+function h = plotColormap(data, a_colormap, num_colors, props)
 
-% plotImage - Function that plots a color matrix on current figure.
+% plotColormap - Colormap plot that requires displaying a colorbar.
 %
 % Usage:
-% h = plotImage(image_data, a_colormap, num_colors, props)
-%
-% Description:
+% h = plotColormap(data, a_colormap, num_colors, props)
 %
 % Parameters:
-%	image_data: 2D matrix with image data.
-%	a_colormap: Colormap vector, function name or handle to colormap (e.g., 'jet').
-% 	num_colors: Parameter to be passed to the a_colormap.
-%	props: A structure with any optional properties.
-%	  colorbar: If defined, show colorbar on plot.
-%	  colorbarProps: Set colorbar axis properties.
-%	  colorbarLabel: Set colorbar y-axis label.
-%	  truncateDecDigits: Truncate labels to this many decimal digits.
-%	  maxValue: Maximal value at num_colors to annotate the colorbar.
-%	  reverseYaxis: If 1, display y-axis values in reverse (default=0).
+%   data: 2D matrix with image data or cell array to be passed as
+%   	arguments to arbitrary plot command (see props).
+%   a_colormap: Colormap vector, function name or handle to colormap (e.g., 'jet').
+%   num_colors: Parameter to be passed to the a_colormap.
+%   props: A structure with any optional properties.
+%     command: Plot command to interpret data (default='image').
+%     colorbar: If given, show colorbar on plot.
+%     colorbarProps: Set colorbar axis properties.
+%     colorbarLabel: Set colorbar y-axis label.
+%     truncateDecDigits: Truncate labels to this many decimal digits.
+%     maxValue: Maximal value at num_colors to annotate the colorbar.
+%     reverseYaxis: If 1, display y-axis values in reverse (default=0).
 %		
 % Returns:
-%	colors: RGB color matrix to be passed to colormap function.
+%   h: Handle to main plot object (e.g., image).
 %
-% See also: colormap
+% Description:
+%   Mainly serves to format the colorbar, which can only be plotted after
+% we prepare the main axis. Thus it cannot be easily integrated into
+% plot_stack. 
 %
-% $Id$
+% See also: colormap, colorbar
 %
 % Author: Cengiz Gunay <cgunay@emory.edu>, 2006/06/05
 
-% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% Copyright (c) 2007-2015 Cengiz Gunay <cengique@users.sf.net>.
 % This work is licensed under the Academic Free License ("AFL")
 % v. 3.0. To view a copy of this license, please look at the COPYING
 % file distributed with this software or visit
 % http://opensource.org/licenses/afl-3.0.php.
 
-h = image(image_data);
+  props = defaultValue('props', struct);
+    
+  % run the plot command
+  command = getFieldDefault(props, 'command', 'image');
+  
+  if ~ iscell(data)
+    h = feval(command, data);
+  else
+    h = feval(command, data{:});
+  end    
 
 if ~ isfield(props, 'reverseYaxis')
   set(gca, 'yDir', 'normal');
@@ -90,7 +102,7 @@ if isfield(props, 'colorbar')
 end
 
 % scale font to fit measure names on y-axis
-num_rows = max(100, size(image_data, 1));
+num_rows = max(100, size(data, 1));
 %set(gca, 'FontUnit', 'normalized', 'FontSize', 1/num_rows);
 
 end
