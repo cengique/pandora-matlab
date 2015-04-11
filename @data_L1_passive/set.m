c2dcdba60b60c11e7f6eaab152480b33d61df3c7
Cengiz Gunay <cengique@users.sf.net>
2015-04-11 21:16:46 +0000

    Summary: added standard class access methods
    git-svn-id: file:///home/cengiz/stg-svnreps/flymotor/trunk/voltage-clamp@837 56a705e8-4ec7-11de-8a84-d56a39cf5c17

---
+++ @data_L1_passive/set.m
@@ -0,0 +1,21 @@
+function a = set(a, varargin)
+% set - Generic method for setting object attributes.
+%
+% Author: Cengiz Gunay <cgunay@emory.edu>, 2004/10/08
+
+% Copyright (c) 2007 Cengiz Gunay <cengique@users.sf.net>.
+% This work is licensed under the Academic Free License ("AFL")
+% v. 3.0. To view a copy of this license, please look at the COPYING
+% file distributed with this software or visit
+% http://opensource.org/licenses/afl-3.0.php.
+attr = varargin{1};
+val = varargin{2};
+if ~ isa(a, 'data_L1_passive')
+  builtin('set', a, attr, val);
+else
+  a.(attr) = val;
+end
+% recurse
+if nargin > 3
+  a = set(a, varargin{3:end});
+end
