1649e3cf4c3f8d004e0e7ecc144f7652867c4680
Cengiz Gunay <cengique@users.sf.net>
2017-06-08 18:26:50 -0400

    handling of missing page names
    only in to_db

--- test_suite/tests_db/addPages_test.m
+++ test_suite/tests_db/addPages_test.m
@@ -31,7 +31,19 @@ function testPageless( testCase )
     assert(b_db.data(1, 1, 1) == 1);
     assert(b_db.data(1, 1, 2) == 1);
     assert(b_db.data(1, 3, 2) == 3);
+    assert(b_db(1, 2, 'page0') == 2);
+    assert(b_db(1, 2, 'page2') == 2);
+
+function testNameless( testCase )
   
+    b_db = addPages(testCase.TestData.a_3_pageless_db, testCase.TestData.a_3_pageless_db);
+    assert(b_db.data(1, 1, 1) == 1);
+    assert(b_db.data(1, 1, 2) == 1);
+    assert(b_db.data(1, 3, 2) == 3);
+    % first page missing because it's not our problem to fix it.
+    % TODO: fix this earlier in tests_3D_db to make sure there are always
+    % page names?
+    %assert(b_db(1, 2, 'page2') == 2); 
   
 function testNon3DObj( testCase )
    
