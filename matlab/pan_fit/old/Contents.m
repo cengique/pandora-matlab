130948f668a6a64e791f9e04f80f942db512804a
Cengiz Gunay <cengique@users.sf.net>
2023-01-30 20:13:40 -0500

    pan_fit folder replaced with submodule

--- matlab/pan_fit/old/Contents.m
+++
@@ -1,4 +0,0 @@
-% param_act_int_v 	- (OBSOLETE) Same as param_act_deriv_v, but buggy.
-% param_I_v 		- (OBSOLETE?) An (non)inactivating current.
-% param_Rs_cap_leak_int_t - (OBSOLETE) Membrane capacitance and leak
-% 	integrated over time.
